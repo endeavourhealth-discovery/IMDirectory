824ac8bb810be5f5aff043a9232b63513058a081
matt-voror <matt@voror.co.uk>
2022-03-21 16:21:30 +0000

    updated scripts for cypress, lint, format. Updated cypress for vite app.

--- tests/e2e/specs/homepage_spec.js
+++ tests/e2e/specs/homepage_spec.js
@@ -1,53 +1,70 @@
 describe("The Home Page", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/api/report/concept/scheme", {
+    cy.intercept("GET", "http://localhost:8080/api/config/public/xmlSchemaDataTypes", {
       statusCode: 200,
       body: [
-        { count: 1029846, label: "Snomed-CT code" },
-        { count: 12079, label: "Term based code" },
-        { count: 1970, label: "Discovery code" },
-        { count: 145, label: "ODS code" },
-        { count: 1, label: "Vision Read2 code scheme"}
+        "http://www.w3.org/2001/XMLSchema#string",
+        "http://www.w3.org/2001/XMLSchema#boolean",
+        "http://www.w3.org/2001/XMLSchema#float",
+        "http://www.w3.org/2001/XMLSchema#double",
+        "http://www.w3.org/2001/XMLSchema#decimal",
+        "http://www.w3.org/2001/XMLSchema#dateTime",
+        "http://www.w3.org/2001/XMLSchema#duration",
+        "http://www.w3.org/2001/XMLSchema#hexBinary",
+        "http://www.w3.org/2001/XMLSchema#base64Binary",
+        "http://www.w3.org/2001/XMLSchema#anyURI",
+        "http://www.w3.org/2001/XMLSchema#ID",
+        "http://www.w3.org/2001/XMLSchema#IDREF",
+        "http://www.w3.org/2001/XMLSchema#ENTITY",
+        "http://www.w3.org/2001/XMLSchema#NOTATION",
+        "http://www.w3.org/2001/XMLSchema#normalizedString",
+        "http://www.w3.org/2001/XMLSchema#token",
+        "http://www.w3.org/2001/XMLSchema#language",
+        "http://www.w3.org/2001/XMLSchema#IDREFS",
+        "http://www.w3.org/2001/XMLSchema#ENTITIES",
+        "http://www.w3.org/2001/XMLSchema#NMTOKEN",
+        "http://www.w3.org/2001/XMLSchema#NMTOKENS",
+        "http://www.w3.org/2001/XMLSchema#Name",
+        "http://www.w3.org/2001/XMLSchema#QName",
+        "http://www.w3.org/2001/XMLSchema#NCName",
+        "http://www.w3.org/2001/XMLSchema#integer",
+        "http://www.w3.org/2001/XMLSchema#nonNegativeInteger",
+        "http://www.w3.org/2001/XMLSchema#positiveInteger",
+        "http://www.w3.org/2001/XMLSchema#nonPositiveInteger",
+        "http://www.w3.org/2001/XMLSchema#negativeInteger",
+        "http://www.w3.org/2001/XMLSchema#byte",
+        "http://www.w3.org/2001/XMLSchema#int",
+        "http://www.w3.org/2001/XMLSchema#long",
+        "http://www.w3.org/2001/XMLSchema#short",
+        "http://www.w3.org/2001/XMLSchema#unsignedByte",
+        "http://www.w3.org/2001/XMLSchema#unsignedInt",
+        "http://www.w3.org/2001/XMLSchema#unsignedLong",
+        "http://www.w3.org/2001/XMLSchema#unsignedShort",
+        "http://www.w3.org/2001/XMLSchema#date",
+        "http://www.w3.org/2001/XMLSchema#time",
+        "http://www.w3.org/2001/XMLSchema#gYearMonth",
+        "http://www.w3.org/2001/XMLSchema#gYear",
+        "http://www.w3.org/2001/XMLSchema#gMonthDay",
+        "http://www.w3.org/2001/XMLSchema#gDay",
+        "http://www.w3.org/2001/XMLSchema#gMonth"
       ]
     });
-    cy.intercept("GET", "/api/report/concept/status", {
+    cy.intercept("GET", "http://localhost:8080/api/config/public/filterDefaults", {
       statusCode: 200,
-      body: [
-        { count: 761788, label: "Active" },
-        { count: 283694, label: "Inactive" },
-        { count: 2050, label: "Draft" },
-      ]
+      body: {
+        schemeOptions: ["http://endhealth.info/im#", "http://snomed.info/sct#"],
+        statusOptions: ["http://endhealth.info/im#Active", "http://endhealth.info/im#Draft"],
+        typeOptions: [
+          "http://endhealth.info/im#Concept",
+          "http://endhealth.info/im#ValueSet",
+          "http://endhealth.info/im#ConceptSet",
+          "http://endhealth.info/im#DataModelEntity",
+          "http://endhealth.info/im#dataModelProperty",
+          "http://endhealth.info/im#Query"
+        ]
+      }
     });
-    cy.intercept("GET", "/api/report/concept/types", {
-      statusCode: 200,
-      body: [
-        { label: "Class", count: 1044018 },
-        { label: "Object Property", count: 1811 },
-        { label: "Set", count: 1122 },
-        { label: "Record Type", count: 94 },
-        { label: "Data Property", count: 68 },
-        { label: "Query Set", count: 45 },
-        { label: "Functional Property", count: 26 },
-        { label: "Annotation Property", count: 23 },
-        { label: "Transitive Property", count: 11 },
-        { label: "Symmetric Property", count: 11 },
-        { label: "Value Set", count: 7 },
-        { label: "Folder", count: 7 },
-        { label: "Reflexive Property", count: 2 },
-        { label: "Query Template", count: 1 },
-        { label: "Named Individual", count: 1 },
-      ]
-    });
-    cy.intercept("GET", "/api/report/concept/category", {
-      statusCode: 200,
-      body: [
-        { label: "Value Sets", count: 7 },
-        { label: "Data Models", count: 1973 },
-        { label: "Ontology", count: 1045267 },
-        { label: "Total", count: 1047247 }
-      ]
-    });
-    cy.visit('/');
+    cy.visit("/");
   });
 
   it("successfully loads", () => {
@@ -63,6 +80,6 @@ describe("The Home Page", () => {
     cy.get(".p-button-label")
       .contains("Agree")
       .click();
-    cy.contains("Ontology overview");
+    cy.url().should("eq", "/");
   });
 });
