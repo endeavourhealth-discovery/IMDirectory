3c1f4c5bec5398d2b018b1f740aa8d0752f30580
Kostas Kardatos <kon.kardatos@gmail.com>
2022-03-30 15:35:52 +0300

    make favourites not expandable

--- src/components/home/NavTree.vue
+++ src/components/home/NavTree.vue
@@ -45,9 +45,6 @@ export default defineComponent({
   name: "NavTree",
   computed: mapState(["conceptIri", "favourites", "locateOnNavTreeIri"]),
   watch: {
-    async favourites() {
-      this.updateFavourites();
-    },
     locateOnNavTreeIri() {
       this.findPathToNode(this.locateOnNavTreeIri);
     },
@@ -84,17 +81,6 @@ export default defineComponent({
     this.loading = false;
   },
   methods: {
-    async updateFavourites() {
-      const favNode = this.findNode(IM.NAMESPACE + "Favourites", this.root);
-
-      favNode.loading = true;
-      favNode.children = [];
-      for (let favourite of this.favourites) {
-        const entity = await EntityService.getPartialEntity(favourite, [RDF.TYPE, RDFS.LABEL, IM.HAS_CHILDREN]);
-        favNode.children.push(this.createTreeNode(entity[RDFS.LABEL], favourite, entity[RDF.TYPE], false));
-      }
-      favNode.loading = false;
-    },
     async addParentFoldersToRoot() {
       const IMChildren = await EntityService.getEntityChildren(IM.NAMESPACE + "InformationModel");
       for (let IMchild of IMChildren) {
@@ -102,17 +88,10 @@ export default defineComponent({
         if (!hasNode) this.root.push(this.createTreeNode(IMchild.name, IMchild["@id"], IMchild.type, IMchild.hasChildren));
       }
       this.root.sort((a, b) => (a.key > b.key ? 1 : b.key > a.key ? -1 : 0));
-      this.root.push({
-        key: "Favourites",
-        label: "Favourites",
-        typeIcon: ["fas", "star"],
-        color: "#e39a36",
-        data: IM.NAMESPACE + "Favourites",
-        leaf: false,
-        loading: false,
-        children: [] as TreeNode[]
-      });
-      this.updateFavourites();
+      const favNode = this.createTreeNode("Favourites", IM.NAMESPACE + "Favourites", [], false);
+      favNode.typeIcon = ["fas", "star"];
+      favNode.color = "#e39a36";
+      this.root.push(favNode);
     },
 
     createTreeNode(conceptName: string, conceptIri: string, conceptTypes: TTIriRef[], hasChildren: boolean): TreeNode {
