8d1b21292958a60596ce1df3dc38ba571372378c
matt-voror <matt@voror.co.uk>
2022-05-23 10:37:27 +0100

    prevented null error in fetching recentactivitydetails

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -127,8 +127,8 @@ export default defineComponent({
       const storedActivity: RecentActivityItem[] = Object.assign([], this.recentLocalActivity);
       for (let activity of storedActivity) {
         const result = await EntityService.getPartialEntity(activity.iri, [RDFS.LABEL, RDF.TYPE]);
-        activity.name = result[RDFS.LABEL];
         if (isObjectHasKeys(result, [RDF.TYPE, RDFS.LABEL])) {
+          activity.name = result[RDFS.LABEL];
           activity.type = result[RDF.TYPE].map((type: TTIriRef) => type.name).join(", ");
         }
       }
