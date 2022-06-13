0f0f3aa479e78d8fa3593032a78f32c48ddda619
matt-voror <matt@voror.co.uk>
2022-06-13 14:46:48 +0100

    refactored shims for vue $ properties

--- src/typings/shims-vue-custom-properties.d.ts
+++ src/typings/shims-vue-custom-properties.d.ts
@@ -1,10 +1,16 @@
 import { ConfigService, EntityService, SetService } from "im-library";
 import { ComponentCustomProperties } from "vue";
+import VueSweetalert2 from "vue-sweetalert2";
+import { Store } from "@/vuex";
+import { Store } from "@/store.index";
+import State from "@/store/stateType";
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $entityService: EntityService;
     $configService: ConfigService;
     $setService: SetService;
+    $swal: VueSweetalert2;
+    $store: Store<State>;
   }
 }
