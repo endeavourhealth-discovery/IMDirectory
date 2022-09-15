bffe75aec424a383253dd978dbb9102846425e76
matt-voror <matt@voror.co.uk>
2022-09-15 14:20:19 +0100

    added msw server to setupTests to cover blocking all fetch requests during testing

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,4 +1,22 @@
+import { setupServer } from "msw/node";
+
 // Global canvas mock (for charts)
 HTMLCanvasElement.prototype.getContext = () => {
-    {}
+  {
+  }
 };
+
+const restHandlers = [];
+const server = setupServer(...restHandlers);
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterAll(() => {
+  server.close();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
