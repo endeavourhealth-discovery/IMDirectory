1fa0de1268adbfd18ccbefaca1d45bfd7a066867
matt-voror <matt@voror.co.uk>
2023-01-16 16:41:27 +0000

    removed evaluateEcl api test code.

--- ui/src/components/directory/topbar/eclSearch/Builder.vue
+++ ui/src/components/directory/topbar/eclSearch/Builder.vue
@@ -94,19 +94,6 @@ watch(
 
 const queryString = ref("");
 
-onMounted(async () => {
-  await processEclString(props.eclString);
-  ecl.value = { type: "BoolGroup", operator: "AND" };
-});
-
-async function processEclString(eclString: string) {
-  const ecl =
-    "<  404684003 |Clinical finding| : {  363698007 |Finding site|  = <<  39057004 |Pulmonary valve structure| , 116676008 |Associated morphology|  = <<  415582006 |Stenosis| }, {  363698007 |Finding site|  = <<  53085002 |Right ventricular structure| , 116676008 |Associated morphology|  = <<  56246009 |Hypertrophy| }";
-  const result = await SetService.evaluateEcl(ecl);
-  console.log("isValidEcl");
-  console.log(result);
-}
-
 function submit(): void {
   emit("ECLSubmitted", queryString.value);
 }
