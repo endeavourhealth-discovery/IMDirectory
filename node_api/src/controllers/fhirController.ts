4b21a2f246e6bbfe3ee0b10d4a5efc493fd91710
Richard Collier <gitkraken@tirinoarim.co.uk>
2023-01-23 17:57:51 +0000

    API Documentation #346

--- node_api/src/controllers/fhirController.ts
+++ node_api/src/controllers/fhirController.ts
@@ -3,29 +3,43 @@ import FhirService from "@/services/fhir.service";
 
 
 export default class FhirController {
-    public path = "/node_api/fhir/r4";
-    public router = express.Router();
-    private service: FhirService;
+  public path = "/node_api/fhir/r4";
+  public router = express.Router();
+  private service: FhirService;
 
-    constructor() {
-        this.initRoutes();
-        this.service = new FhirService();
-    }
+  constructor() {
+    this.initRoutes();
+    this.service = new FhirService();
+  }
 
-    private initRoutes() {
-        this.router.get("/ValueSet", (req,res, next) => this.getValueSet(req, res, next,false));
-        this.router.get("/ValueSet/[\$]expand", (req,res, next) => this.getValueSet(req, res, next,true));
-    }
+  private initRoutes() {
 
-    async getValueSet(req: Request, res: Response, next: NextFunction,expand:boolean) {
-        try {
-            const data = await this.service.getValueSet(req.query.url as string, expand);
-            res
-                .setHeader('content-type','application/fhir+json')
-                .send(data)
-                .end();
-        } catch (e) {
-            next(e);
-        }
+    this.router.get("/ValueSet", (req, res, next) => {
+      /*
+        #swagger.summary = 'Retrieves the specified value set'
+        #swagger.parameters['url'] = { in: 'query', description: 'url/iri of the value set' }
+      */
+      return this.getValueSet(req, res, next, false)
+    });
+    this.router.get("/ValueSet/[\$]expand", (req, res, next) => {
+      /*
+        #swagger.path = '/ValueSet/$expand'
+        #swagger.summary = 'Retrieves the specified value set and expands any subsets & members'
+        #swagger.parameters['url'] = { in: 'query', description: 'url/iri of the value set' }
+      */
+      return this.getValueSet(req, res, next, true)
+    });
+  }
+
+  async getValueSet(req: Request, res: Response, next: NextFunction,expand:boolean) {
+    try {
+      const data = await this.service.getValueSet(req.query.url as string, expand);
+      res
+        .setHeader('content-type','application/fhir+json')
+        .send(data)
+        .end();
+    } catch (e) {
+      next(e);
     }
+  }
 }
