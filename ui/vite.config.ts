c936e9673a659279c9799aa8a69405d911dd552e
matt-voror <matt@voror.co.uk>
2023-02-07 19:30:54 +0000

    fixes for node_api to run antlr4 es6 build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     port: 8082,
     proxy: {
       "/nodeapi": {
-        target: "http://127.0.0.1:3000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
         ws: true,
