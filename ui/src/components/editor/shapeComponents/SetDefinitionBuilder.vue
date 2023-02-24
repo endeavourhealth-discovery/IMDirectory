7912b5c492124496e36052e089560f0eaa16fea2
matt-voror <matt@voror.co.uk>
2023-02-24 17:47:19 +0000

    added addByCode and fixed component to handle csv files correctly.

--- ui/src/components/editor/shapeComponents/SetDefinitionBuilder.vue
+++ ui/src/components/editor/shapeComponents/SetDefinitionBuilder.vue
@@ -2,6 +2,10 @@
   <div class="set-definition-container">
     <div class="ecl-container">
       <div class="text-copy-container">
+        <!-- <div class="field-checkbox">
+          <Checkbox inputId="showNames" v-model="showNames" :binary="true" />
+          <label for="showNames">Show names</label>
+        </div> -->
         <Textarea
           v-model="ecl"
           id="ecl-string-container"
@@ -12,6 +16,8 @@
         />
       </div>
       <div class="button-container">
+        <Button label="Import" @click="toggleMenuOptions" aria-haspopup="true" aria-controls="import_menu" />
+        <Menu id="import_menu" ref="importMenu" :model="buttonOptions" :popup="true" />
         <Button :disabled="eclError" label="ECL builder" @click="showBuilder" class="p-button-help" data-testid="builder-button" />
         <Button
           icon="fa-solid fa-copy"
@@ -32,11 +38,13 @@
     @closeDialog="$event => (showDialog = false)"
     @eclConversionError="updateError"
   />
+  <AddByCodeList :showAddByFile="showAddByFileDialog" :showAddByList="showAddByCodeListDialog" @closeDialog="closeAddByDialog" @addCodeList="processCodeList" />
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, watch, Ref, PropType, inject } from "vue";
 import Builder from "@/components/directory/topbar/eclSearch/Builder.vue";
+import AddByCodeList from "./setDefinition/AddByCodeList.vue";
 import { EditorMode } from "@im-library/enums";
 import { EclService } from "@/services";
 import _ from "lodash";
@@ -45,6 +53,8 @@ import { PropertyGroup, Query, TTAlias } from "@im-library/models/AutoGen";
 import { useToast } from "primevue/usetoast";
 import { ToastOptions } from "@im-library/models";
 import { ToastSeverity } from "@im-library/enums";
+import { isArrayHasLength } from "@im-library/helpers/DataTypeCheckers";
+import { ConceptSummary } from "@im-library/interfaces";
 
 const props = defineProps({
   shape: { type: Object as PropType<PropertyGroup>, required: true },
@@ -54,18 +64,28 @@ const props = defineProps({
 
 const toast = useToast();
 
+const importMenu = ref();
+
 const ecl: Ref<string> = ref("");
+// const eclNoNames = ref("");
 const eclAsQuery: Ref<Query | undefined> = ref();
 const showDialog = ref(false);
+const showAddByCodeListDialog = ref(false);
+const showAddByFileDialog = ref(false);
 const eclError = ref(false);
 const eclErrorMessage = ref("");
 const loading = ref(false);
+const showNames = ref(false);
 
 const entityUpdate = inject(injectionKeys.editorEntity)?.updateEntity;
 const editorEntity = inject(injectionKeys.editorEntity)?.editorEntity;
 const validityUpdate = inject(injectionKeys.editorValidity)?.updateValidity;
 
 const key = props.shape.path["@id"];
+const buttonOptions = [
+  { label: "From list", command: () => showAddByCodeList() },
+  { label: "From file", command: () => showAddByFile() }
+];
 
 watch(
   () => props.value,
@@ -77,6 +97,7 @@ watch(
 );
 
 watch(ecl, async newValue => {
+  // eclNoNames.value = ecl.value.replace(/\|.*?\|/g, "").replace(/\s\s+/g, " ");
   loading.value = true;
   if (await EclService.isValidECL(newValue)) {
     eclAsQuery.value = await EclService.getQueryFromECL(newValue);
@@ -100,8 +121,39 @@ onMounted(async () => {
   }
 });
 
+function toggleMenuOptions(event: any) {
+  importMenu.value.toggle(event);
+}
+
 function showBuilder(): void {
   showDialog.value = true;
+}
+
+function showAddByCodeList(): void {
+  showAddByCodeListDialog.value = true;
+}
+
+function showAddByFile(): void {
+  showAddByFileDialog.value = true;
+}
+
+function closeAddByDialog(): void {
+  showAddByCodeListDialog.value = false;
+  showAddByFileDialog.value = false;
+}
+
+function processCodeList(data: ConceptSummary[]) {
+  closeAddByDialog();
+  if (isArrayHasLength(data)) {
+    let arrayAsStrings = data.map(item => {
+      let itemAsString = "";
+      itemAsString += "<< ";
+      itemAsString += item.code;
+      if (item.name) itemAsString += " | " + item.name + " | ";
+      return itemAsString;
+    });
+    ecl.value = arrayAsStrings.join(" AND ");
+  }
 }
 
 async function updateValidity() {
@@ -174,7 +226,7 @@ function updateError(errorUpdate: { error: boolean; message: string }): void {
   flex: 1 1 auto;
   width: 100%;
   display: flex;
-  flex-flow: row;
+  flex-flow: column;
   align-items: center;
   margin: 0 0 1rem 0;
 }
