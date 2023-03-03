0fde904589a86d4dd0bd61d179435326ca476403
matt-voror <80042589+matt-voror@users.noreply.github.com>
2023-03-03 15:41:25 +0000

    Revert "clean install also deletes and regenerates package-lock files"

--- ui/src/fontAwesomeProIcons/addSharpSolidIcons.js
+++
@@ -1,8 +0,0 @@
-export default async function addSharpSolidIcons(library) {
-  // #v-ifdef VITE_FONT_AWESOME_PACKAGE_TOKEN
-  await Promise.all([
-    /*@ts-ignore*/
-    import(/*@vite-ignore*/ "@fortawesome/sharp-solid-svg-icons/faLightbulbSlash").then(({ faLightbulbSlash }) => library.add(faLightbulbSlash))
-  ]);
-  // #v-endif
-}
