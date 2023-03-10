e799236e005278d24003df5dde34d0f7fe8a2173
Kostas Kardatos <kon.kardatos@gmail.com>
2023-03-10 14:00:18 +0200

    delete unused setQueryObject

--- im_library/src/interfaces/queryBuilder/SetQueryObject.ts
+++
@@ -1,9 +0,0 @@
-import { TTAlias } from "../../models/AutoGen";
-import { Refinement } from "./Refinement";
-
-export interface SetQueryObject {
-  include: boolean;
-  concept: TTAlias;
-  refinements: Refinement[];
-  isType?: boolean;
-}
