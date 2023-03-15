b6b4a66f052a8b1a01b4556851fca5613765a2b2
Kostas Kardatos <kon.kardatos@gmail.com>
2023-03-15 15:52:35 +0200

    add button bar

--- ui/src/views/Query.vue
+++ ui/src/views/Query.vue
@@ -8,11 +8,20 @@
     <div id="query-main-container">
       <RecursiveTableQuery :bool="'and'" :query-data="data" :selected="selected" :level="0" />
     </div>
-    {{ query }}
+    <div class="button-bar">
+      <Button class="button-bar-button" label="Run" />
+      <Button class="button-bar-button" label="View" severity="secondary" @click="visibleDialog = true" />
+      <Button class="button-bar-button" label="Save" severity="success" />
+    </div>
   </div>
+  <Dialog v-model:visible="visibleDialog" modal header="Header" :style="{ width: '50vw' }">
+    <VueJsonPretty class="json" :path="'res'" :data="query" @nodeClick="copy" />
+  </Dialog>
 </template>
 
 <script setup lang="ts">
+import VueJsonPretty from "vue-json-pretty";
+import "vue-json-pretty/lib/styles.css";
 import TopBar from "@/components/shared/TopBar.vue";
 import { ref, Ref, onMounted } from "vue";
 import { queryDefinition } from "./query-json";
@@ -20,11 +29,15 @@ import RecursiveTableQuery from "../components/query/RecursiveTableQuery.vue";
 import { TableQuery } from "@im-library/interfaces";
 import { buildTableQuery } from "@im-library/helpers/TableQueryBuilder";
 import { useStore } from "vuex";
-
+import { useToast } from "primevue/usetoast";
+import { ToastOptions } from "@im-library/models";
+import { ToastSeverity } from "@im-library/enums";
+const toast = useToast();
 const store = useStore();
 const selected = ref([] as any[]);
 const data: Ref<TableQuery[]> = ref({} as TableQuery[]);
 const query: Ref<any> = ref();
+const visibleDialog: Ref<boolean> = ref(false);
 
 function getTableQuery() {
   query.value = { ...queryDefinition };
@@ -38,6 +51,11 @@ onMounted(async () => {
 
   getTableQuery();
 });
+
+async function copy() {
+  await navigator.clipboard.writeText(JSON.stringify(query.value));
+  toast.add(new ToastOptions(ToastSeverity.SUCCESS, "JSON value copied to clipboard"));
+}
 </script>
 
 <style scoped lang="scss">
@@ -61,5 +79,14 @@ onMounted(async () => {
   display: flex;
   flex-flow: row;
   border: 2px solid #b89241;
+}
+
+.button-bar {
+  display: flex;
+  justify-content: end;
+}
+
+.button-bar-button {
+  margin: 0.5rem;
 }
 </style>
