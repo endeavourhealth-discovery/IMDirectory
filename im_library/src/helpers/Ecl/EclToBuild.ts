efc5e050f9c0ed792fc149f87c92528db3819666
matt-voror <matt@voror.co.uk>
2023-03-28 16:48:53 +0100

    updated eclTo helpers to throw custom errors on failure

--- im_library/src/helpers/Ecl/EclToBuild.ts
+++ im_library/src/helpers/Ecl/EclToBuild.ts
@@ -1,9 +1,16 @@
 import ECLBuilderVisitor from "../../antlr4/ecl/ECLBuilderVisitor";
+import { ErrorType } from "../../enums";
+import { CustomError } from "../../models";
 import setupTree from "./SetupTree";
 
 export function eclToBuild(ecl: string) {
-  const tree = setupTree(ecl);
-  const visitor = new ECLBuilderVisitor();
-  const result = visitor.visit(tree);
-  return result;
+  try {
+    const tree = setupTree(ecl);
+    const visitor = new ECLBuilderVisitor();
+    const result = visitor.visit(tree);
+    return result;
+  } catch (error) {
+    if (error instanceof Error) throw new CustomError(error.message, ErrorType.InvalidEclError);
+    else throw error;
+  }
 }
