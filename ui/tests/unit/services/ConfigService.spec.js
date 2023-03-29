60194438731efc3775c64284c667546f8dda7dee
matt-voror <matt@voror.co.uk>
2023-03-29 16:44:59 +0100

    updated tests by deleting axios fail tests

--- ui/tests/unit/services/ConfigService.spec.js
+++ ui/tests/unit/services/ConfigService.spec.js
@@ -23,27 +23,3 @@ describe("ConfigService.ts ___ axios success", () => {
     expect(result).toStrictEqual(["test config"]);
   });
 });
-
-describe("ConfigService.ts ___ axios fail", () => {
-  const node_api = Env.VITE_NODE_API;
-
-  beforeEach(() => {
-    vi.resetAllMocks();
-    const mockError = new Error("axios test error");
-    axios.get = vi.fn().mockRejectedValue(mockError);
-  });
-
-  it("can get component layout", async () => {
-    const result = await ConfigService.getComponentLayout("definition");
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(node_api + "node_api/config/public/componentLayout", { params: { name: "definition" } });
-    expect(result).toStrictEqual([]);
-  });
-
-  it("can get dashboardLayout", async () => {
-    const result = await ConfigService.getDashboardLayout("conceptDashboard");
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(node_api + "node_api/config/public/dashboardLayout", { params: { name: "conceptDashboard" } });
-    expect(result).toStrictEqual([]);
-  });
-});
