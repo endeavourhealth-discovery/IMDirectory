5b613841229f8ad7be94502b08086c4844308427
Kostas Kardatos <kon.kardatos@gmail.com>
2023-03-31 15:47:45 +0300

    Value set members to display standard summary overlay Fixes #571

--- ui/src/components/shared/IMViewerLink.vue
+++ ui/src/components/shared/IMViewerLink.vue
@@ -1,16 +1,19 @@
 <template>
-  <a class="clickable" @click="click()">{{ label || iri }}</a>
+  <a class="clickable" @click="click()" @mouseover="showOverlay($event)" @mouseleave="hideOverlay($event)">{{ label || iri }}</a>
+  <OverlaySummary ref="OS" />
 </template>
 
 <script setup lang="ts">
+import { Ref, ref } from "vue";
 import { DirectService } from "../../services";
+import OverlaySummary from "../directory/viewer/OverlaySummary.vue";
 
 const props = defineProps({
   iri: { type: String, required: true },
   label: { type: String, required: false },
   action: { type: String, required: false, default: "view" }
 });
-
+const OS: Ref<any> = ref();
 const directService = new DirectService();
 
 async function click() {
@@ -28,6 +31,14 @@ async function click() {
       directService.view(props.iri);
       break;
   }
+}
+
+async function showOverlay(event: any): Promise<void> {
+  await OS.value.showOverlay(event, props.iri);
+}
+
+function hideOverlay(event: any): void {
+  OS.value.hideOverlay(event);
 }
 </script>
 
