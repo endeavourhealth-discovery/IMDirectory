9f610a70396a3faac3224789564671ed620330a3
Kostas Kardatos <kon.kardatos@gmail.com>
2023-04-06 15:46:47 +0300

    Query tab not showing query display tree properly Fixes #606

--- im_library/src/helpers/DisplayQueryBuilder.ts
+++ im_library/src/helpers/DisplayQueryBuilder.ts
@@ -1,5 +1,5 @@
 import { DisplayQuery } from "../interfaces";
-import { Element, Where } from "../interfaces/AutoGen";
+import { Element, Match, Path, Where, Property, OrderLimit } from "../interfaces/AutoGen";
 import { isArrayHasLength, isObjectHasKeys } from "./DataTypeCheckers";
 
 export function buildDisplayQuery(query: any) {
@@ -9,12 +9,12 @@ export function buildDisplayQuery(query: any) {
 }
 
 function buildRecursively(query: any, type: string, parent: DisplayQuery) {
-  if ("where" === type || "with" === type) {
-    addWhere(query, type, parent);
-  } else if ("select" === type) {
-    addSelect(query, type, parent);
+  if ("match" === type) {
+    addMatch(query, type, parent);
   } else if (isObjectHasKeys(query)) {
-    addObject(query, type, parent);
+    for (const key of Object.keys(query)) {
+      if (!isPrimitiveType(query[key])) buildRecursively(query[key], key, parent);
+    }
   } else if (isArrayHasLength(query)) {
     for (const nested of query) {
       buildRecursively(nested, type, parent);
@@ -34,72 +34,84 @@ function buildDQInstance(parent: DisplayQuery, label: string, type?: string, dat
 }
 
 // adders
-function addObject(query: any, type: string, parent: DisplayQuery) {
-  const label = query.name || query.id || query.bool || query.description || query.variable || getNameFromRef(query);
-  let displayQuery;
-  if (label && label !== "and") {
-    displayQuery = buildDQInstance(parent, label, type, query);
-    parent.children.push(displayQuery);
-  }
-  for (const key of Object.keys(query)) {
-    if (!isPrimitiveType(query[key])) buildRecursively(query[key], displayQuery ? key : type, displayQuery ? displayQuery : parent);
+function addMatch(match: any, type: string, parent: DisplayQuery) {
+  if (isArrayHasLength(match)) {
+    for (const matchItem of match) {
+      addMatch(matchItem, type, parent);
+    }
+  } else if (!isObjectHasKeys(match, ["match", "boolMatch"])) {
+    const label = getMatchLabel(match);
+    const matchParent = addItem(label, match, type, parent);
+    if (!matchParent.label && match.description) matchParent.label = match.description;
+
+    if (isObjectHasKeys(match, ["where"])) {
+      addMatchWhere(match.where, "where", matchParent);
+    }
+    if (isObjectHasKeys(match, ["orderBy"])) {
+      addMatchOrderBy(match.orderBy, "orderBy", matchParent);
+    }
+  } else if (isArrayHasLength(match.match)) {
+    const boolParent = addItem(match.boolMatch, match, type, parent);
+    for (const matchItem of match.match) {
+      addMatch(matchItem, type, boolParent);
+    }
   }
 }
 
-function addWhere(query: any, type: string, parent: DisplayQuery) {
-  if (isAnd(query)) {
-    for (const where of query.where) {
-      if (query.exclude) where.exclude = true;
-      buildRecursively(where, "where", parent);
-    }
-  } else if (isLeafWhere(query) && (isObjectHasKeys(query, ["@id"]) || isObjectHasKeys(query, ["id"]) || isObjectHasKeys(query, ["bool", "in"]))) {
-    const id = query.id || query.name || query["@id"];
-    if (isObjectHasKeys(query, ["in"])) {
-      addInClause(id, query, type, parent);
-    } else if (isComparisonWhere(query)) {
-      const label = id + " " + getComparisonLabel(query);
-      addItem(label, query, type, parent);
-    } else if (isObjectHasKeys(query, ["notExist"])) {
-      const label = `${id} does not exist`;
-      addItem(label, query, type, parent);
-    } else if (isObjectHasKeys(query, ["range"])) {
-      const from = " from " + getComparisonLabel(query.range.from);
-      const to = " to " + getComparisonLabel(query.range.from);
-      const label = id + " " + from + to;
-      addItem(label, query, type, parent);
-    } else {
-      addObject(query, type, parent);
+function addMatchOrderBy(orderBy: any, type: string, parent: DisplayQuery) {
+  if (isArrayHasLength(orderBy)) {
+    for (const orderByItem of orderBy) {
+      addMatchOrderBy(orderByItem, type, parent);
     }
   } else {
-    addObject(query, type, parent);
+    const orderByItem: OrderLimit = orderBy;
+    if (isObjectHasKeys(orderByItem, ["@id", "direction", "limit"]) && 1 === orderByItem.limit) {
+      const label = "order by: " + orderByItem.node;
+      addItem(label, orderBy, type, parent);
+    }
   }
 }
 
-function addSelect(query: any, type: string, parent: DisplayQuery) {
-  if (isArrayHasLength(query)) {
-    const child = buildDQInstance(parent, type, type, query);
-    parent.children.push(child);
-    for (const item of query) {
-      buildRecursively(item, type, child);
+function addMatchWhere(where: any, type: string, parent: DisplayQuery) {
+  if (isArrayHasLength(where)) {
+    for (const whereItem of where) {
+      addMatchWhere(whereItem, type, parent);
     }
   } else {
-    const label = getNameFromRef(query);
-    const child = addItem(label, query, type, parent);
-    if (isObjectHasKeys(query, ["where"])) {
-      buildRecursively(query.where, "where", child);
+    if (!parent.label && where.node) parent.label += where.node;
+    if (isObjectHasKeys(where, ["in"])) {
+      addIn(where, parent);
     }
-    if (isObjectHasKeys(query, ["with"])) {
-      buildRecursively(query.with, "with", child);
+    if (isObjectHasKeys(where, ["range"])) {
+      addRange(where, type, parent);
+    }
+    if (isObjectHasKeys(where, ["operator"])) {
+      addComparison(where, type, parent);
     }
   }
 }
 
-function addInItems(label: string, query: any, type: string, parent: DisplayQuery) {
-  const child = buildDQInstance(parent, label, type, query);
-  parent.children.push(child);
-  for (const inItem of query.in) {
-    addItem(getNameFromRef(inItem), inItem, "whereIn", child);
+function addComparison(where: any, type: string, parent: DisplayQuery) {
+  const label = getNameFromRef(where) + " " + getComparisonLabel(where);
+  addItem(label, where, type, parent);
+}
+
+function addIn(where: any, parent: DisplayQuery) {
+  parent.label += "." + getNameFromRef(where);
+  if (isArrayHasLength(where.in) && where.in.length === 1) {
+    parent.label += ": " + getNameFromRef(where.in[0]);
+  } else {
+    for (const inItem of where.in as Element[]) {
+      addItem(getNameFromRef(inItem), inItem, "in", parent);
+    }
   }
+}
+
+function addRange(where: any, type: string, parent: DisplayQuery) {
+  const from = " from " + getComparisonLabel(where.range.from);
+  const to = " to " + getComparisonLabel(where.range.from);
+  const label = getNameFromRef(where) + " " + from + to;
+  addItem(label, where, type, parent);
 }
 
 function addItem(label: string, query: any, type: string, parent: DisplayQuery) {
@@ -108,28 +120,15 @@ function addItem(label: string, query: any, type: string, parent: DisplayQuery) 
   return child;
 }
 
-function addInClause(id: string, query: any, type: string, parent: DisplayQuery) {
-  let label = getInLabel(id, query, type, parent);
-  if (isArrayHasLength(query.in) && query.in.length > 1) {
-    label += query.valueLabel ? ": " + query.valueLabel + " (expand to see more...)" : " from";
-    addInItems(label, query, type, parent);
-  } else if (isObjectHasKeys(query.in[0], ["where"])) {
-    label += " from";
-    const child = addItem(label, query, type, parent);
-    addWhere(query.in[0].where, "where", child);
-  } else {
-    label += ": " + (query.valueLabel || getNameFromRef(query.in[0]));
-    addItem(label, query, type, parent);
-  }
-}
-
 // getters
-function getInLabel(id: string, query: any, type: string, parent: DisplayQuery) {
-  let label = "";
-  if (isObjectHasKeys(query, ["in", "latest", "count"])) label += "Latest " + id;
-  else if (hasBool(query, "not") && isObjectHasKeys(query, ["bool", "in"])) label += "Not from";
-  else label += id;
-
+function getMatchLabel(match: Match) {
+  let label: string = getNameFromRef(match);
+  if (!label && match.path) {
+    for (const path of match.path as Element[]) {
+      label += getNameFromRef(path) + ".";
+    }
+    label = label.substring(0, label.length - 1);
+  }
   return label;
 }
 
@@ -137,57 +136,42 @@ function getKey(parent: DisplayQuery) {
   return parent.key + parent.children.length;
 }
 
-function getNameFromRef(ref: Element) {
+function getNameFromRef(ref: any) {
+  let name = "";
   if (isObjectHasKeys(ref, ["name"])) {
-    return ref.name;
+    name = ref.name;
   } else if (isObjectHasKeys(ref, ["@id"])) {
     const splits = ref["@id"].split("#");
-    return splits[1] || splits[0];
+    name = splits[1] || splits[0];
   } else if (isObjectHasKeys(ref, ["@set"])) {
     const splits = ref["@set"].split("#");
-    return splits[1] || splits[0];
+    name = splits[1] || splits[0];
   } else if (isObjectHasKeys(ref, ["@type"])) {
     const splits = ref["@type"].split("#");
-    return splits[1] || splits[0];
+    name = splits[1] || splits[0];
   }
-  return "";
+
+  if (ref.variable) name += "(as " + ref.variable + ")";
+  return name;
 }
 
 function getComparisonLabel(query: any) {
   let label = "";
   if (query.operator) label += query.operator;
-  if (query.relativeTo) label += " " + query.relativeTo;
+  if (query.relativeTo) label += " " + getLabelFromProperty(query.relativeTo);
   if (query.value) label += " " + query.value;
   if (query.unit) label += " (" + query.unit + ")";
   return label;
 }
 
+function getLabelFromProperty(property: Property) {
+  return property.parameter || property.node || getNameFromRef(property);
+}
+
 // checkers
-function hasBool(where: any, value?: string) {
-  if (value) return isObjectHasKeys(where, ["bool"]) && value === where.bool;
-  return isObjectHasKeys(where, ["bool"]);
-}
-
-function isAnd(where: Where) {
-  return hasBool(where, "and") && isArrayHasLength(where.where);
-}
-
-function isLeafWhere(where: any) {
-  return !isArrayHasLength(where) && !isObjectHasKeys(where, ["where", "with"]);
-}
-
 function isPrimitiveType(object: any) {
   const primitiveTypes = ["string", "number", "boolean"];
   return primitiveTypes.includes(typeof object);
-}
-
-function isComparisonWhere(where: Where) {
-  return (
-    isObjectHasKeys(where, ["operator", "unit", "value", "relativeTo"]) ||
-    isObjectHasKeys(where, ["operator", "unit", "value"]) ||
-    isObjectHasKeys(where, ["operator", "value"]) ||
-    isObjectHasKeys(where, ["operator", "relativeTo"])
-  );
 }
 
 export default { buildDisplayQuery };
--- im_library/src/interfaces/AutoGen.ts
+++ im_library/src/interfaces/AutoGen.ts
@@ -6,397 +6,395 @@
  * Structure containing search request parameters and filters
  */
 export interface SearchRequest {
-    /**
-     * Plain text, space separated list of terms
-     */
-    termFilter: string;
-    index: string;
-    /**
-     * List of entity status IRI's
-     */
-    statusFilter: string[];
-    /**
-     * List of entity type IRI's
-     */
-    typeFilter: string[];
-    /**
-     * List of code scheme IRI's
-     */
-    schemeFilter: string[];
-    /**
-     * Marks the results if they are descendants of any of these entities, but does not filter by them
-     */
-    markIfDescendentOf: string[];
-    /**
-     * List of IRIs that must be supertypes of the matches
-     */
-    isA: string[];
-    /**
-     * The search result page number to retrieve
-     */
-    page: number;
-    /**
-     * The number of results to retrieve per page
-     */
-    size: number;
-    from: number;
-    /**
-     * list of fields or property paths from search result summary to return 
-     */
-    select: string[];
-    sortField: string;
-    sortDirection: string;
+  /**
+   * Plain text, space separated list of terms
+   */
+  termFilter: string;
+  index: string;
+  /**
+   * List of entity status IRI's
+   */
+  statusFilter: string[];
+  /**
+   * List of entity type IRI's
+   */
+  typeFilter: string[];
+  /**
+   * List of code scheme IRI's
+   */
+  schemeFilter: string[];
+  /**
+   * Marks the results if they are descendants of any of these entities, but does not filter by them
+   */
+  markIfDescendentOf: string[];
+  /**
+   * List of IRIs that must be supertypes of the matches
+   */
+  isA: string[];
+  /**
+   * The search result page number to retrieve
+   */
+  page: number;
+  /**
+   * The number of results to retrieve per page
+   */
+  size: number;
+  from: number;
+  /**
+   * list of fields or property paths from search result summary to return
+   */
+  select: string[];
+  sortField: string;
+  sortDirection: string;
 }
 
 export interface SearchResponse {
-    page: number;
-    count: number;
-    entities: SearchResultSummary[];
+  page: number;
+  count: number;
+  entities: SearchResultSummary[];
 }
 
 export interface Binding {
-    predicateBinding: { [index: string]: string };
-    predicateObject: { [index: string]: Binding };
+  predicateBinding: { [index: string]: string };
+  predicateObject: { [index: string]: Binding };
 }
 
 export interface Concept extends Entity {
-    subClassOf: TTIriRef[];
-    code: string;
-    im1Id: string[];
-    matchedFrom: Concept[];
-    usage: number;
+  subClassOf: TTIriRef[];
+  code: string;
+  im1Id: string[];
+  matchedFrom: Concept[];
+  usage: number;
 }
 
 export interface ConceptSet extends Entity {
-    definition: Query;
-    hasMember: TTIriRef[];
-    usedIn: TTIriRef[];
+  definition: Query;
+  hasMember: TTIriRef[];
+  usedIn: TTIriRef[];
 }
 
 export interface Entity {
-    "@id": string;
-    type: TTIriRef[];
-    status: TTIriRef;
-    scheme: TTIriRef;
-    isContainedIn: TTIriRef[];
-    name: string;
-    description: string;
+  "@id": string;
+  type: TTIriRef[];
+  status: TTIriRef;
+  scheme: TTIriRef;
+  isContainedIn: TTIriRef[];
+  name: string;
+  description: string;
 }
 
 export interface FormGenerator {
-    "@id": string;
-    status: TTIriRef;
-    label: string;
-    comment: string;
-    targetShape: TTIriRef;
-    type: TTIriRef[];
-    isContainedIn: TTIriRef[];
-    subClassOf: TTIriRef[];
-    group: PropertyGroup[];
-    scheme: TTIriRef;
-    iri: string;
+  "@id": string;
+  status: TTIriRef;
+  label: string;
+  comment: string;
+  targetShape: TTIriRef;
+  type: TTIriRef[];
+  isContainedIn: TTIriRef[];
+  subClassOf: TTIriRef[];
+  group: PropertyGroup[];
+  scheme: TTIriRef;
+  iri: string;
 }
 
 export interface FunctionRequest {
-    functionIri: string;
-    arguments: Argument[];
+  functionIri: string;
+  arguments: Argument[];
 }
 
 export interface MapFunction extends TTIriRef {
-    argument: Argument[];
-    conceptMap: { [index: string]: string };
-    defaultValue: TTIriRef;
+  argument: Argument[];
+  conceptMap: { [index: string]: string };
+  defaultValue: TTIriRef;
 }
 
 export interface ModelDocument {
-    "@context": TTContext;
-    query: QueryEntity[];
-    folder: Entity[];
-    conceptSet: ConceptSet[];
-    function: MapFunction[];
+  "@context": TTContext;
+  query: QueryEntity[];
+  folder: Entity[];
+  conceptSet: ConceptSet[];
+  function: MapFunction[];
 }
 
 export interface NodeShape extends TTIriRef {
-    property: PropertyShape[];
+  property: PropertyShape[];
 }
 
 export interface Page {
-    pageNumber: number;
-    pageSize: number;
+  pageNumber: number;
+  pageSize: number;
 }
 
 export interface Path {
-    source: TTTypedRef;
-    items: TTTypedRef[];
-    target: TTTypedRef;
+  source: TTTypedRef;
+  items: TTTypedRef[];
+  target: TTTypedRef;
 }
 
 export interface PropertyGroup {
-    label: string;
-    comment: string;
-    name: string;
-    order: number;
-    minCount: number;
-    maxCount: number;
-    property: PropertyShape[];
-    componentType: TTIriRef;
-    subGroup: PropertyGroup[];
-    path: TTIriRef;
-    validation: TTIriRef;
-    validationErrorMessage: string;
-    function: TTIriRef;
-    valueIri: TTIriRef;
-    builderChild: boolean;
-    argument: Argument[];
+  label: string;
+  comment: string;
+  name: string;
+  order: number;
+  minCount: number;
+  maxCount: number;
+  property: PropertyShape[];
+  componentType: TTIriRef;
+  subGroup: PropertyGroup[];
+  path: TTIriRef;
+  validation: TTIriRef;
+  validationErrorMessage: string;
+  function: TTIriRef;
+  valueIri: TTIriRef;
+  builderChild: boolean;
+  argument: Argument[];
 }
 
 export interface PropertyShape {
-    label: string;
-    comment: string;
-    name: string;
-    order: number;
-    minCount: number;
-    maxCount: number;
-    componentType: TTIriRef;
-    path: TTIriRef;
-    node: TTIriRef[];
-    validation: TTIriRef;
-    search: TTIriRef;
-    select: TTIriRef[];
-    argument: Argument[];
-    valueVariable: string;
-    isIri: TTIriRef;
-    isTextValue: string;
-    isNumericValue: string;
-    forceIsValue: boolean;
-    builderChild: boolean;
-    datatype: TTIriRef;
-    clazz: TTIriRef;
-    validationErrorMessage: string;
-    function: TTIriRef;
-    expression: NodeShape;
-    subProperty: PropertyShape[];
+  label: string;
+  comment: string;
+  name: string;
+  order: number;
+  minCount: number;
+  maxCount: number;
+  componentType: TTIriRef;
+  path: TTIriRef;
+  node: TTIriRef[];
+  validation: TTIriRef;
+  search: TTIriRef;
+  select: TTIriRef[];
+  argument: Argument[];
+  valueVariable: string;
+  isIri: TTIriRef;
+  isTextValue: string;
+  isNumericValue: string;
+  forceIsValue: boolean;
+  builderChild: boolean;
+  datatype: TTIriRef;
+  clazz: TTIriRef;
+  validationErrorMessage: string;
+  function: TTIriRef;
+  expression: NodeShape;
+  subProperty: PropertyShape[];
 }
 
 export interface TransformRequest {
-    transformMap: TTIriRef;
-    sourceFormat: string;
-    targetFormat: string;
-    source: { [index: string]: any[] };
+  transformMap: TTIriRef;
+  sourceFormat: string;
+  targetFormat: string;
+  source: { [index: string]: any[] };
 }
 
 export interface Argument {
-    parameter: string;
-    valueData: string;
-    valueVariable: string;
-    valueIri: TTIriRef;
-    valueIriList: TTIriRef[];
-    valueDataList: string[];
-    valueObject: any;
+  parameter: string;
+  valueData: string;
+  valueVariable: string;
+  valueIri: TTIriRef;
+  valueIriList: TTIriRef[];
+  valueDataList: string[];
+  valueObject: any;
 }
 
 export interface Assignable {
-    value: string;
-    operator: Operator;
-    relativeTo: Property;
-    unit: string;
+  value: string;
+  operator: Operator;
+  relativeTo: Property;
+  unit: string;
 }
 
 export interface Case {
-    range: Range;
-    value: string;
-    outputData: string;
-    outputIri: TTIriRef;
+  range: Range;
+  value: string;
+  outputData: string;
+  outputIri: TTIriRef;
 }
 
 export interface Delete {
-    where: Where;
-    subject: Element;
-    inverse: boolean;
-    predicate: Element;
-    object: Element;
-    delete: Delete[];
+  where: Where;
+  subject: Element;
+  inverse: boolean;
+  predicate: Element;
+  object: Element;
+  delete: Delete[];
 }
 
 export interface Element extends Entailment {
-    "@id": string;
-    "@type": string;
-    "@set": string;
-    variable: string;
-    name: string;
-    parameter: string;
-    inverse: boolean;
-    iri: string;
+  "@id": string;
+  "@type": string;
+  "@set": string;
+  variable: string;
+  name: string;
+  parameter: string;
+  inverse: boolean;
+  iri: string;
 }
 
 export interface Entailment {
-    descendantsOf: boolean;
-    descendantsOrSelfOf: boolean;
-    ancestorsOf: boolean;
+  descendantsOf: boolean;
+  descendantsOrSelfOf: boolean;
+  ancestorsOf: boolean;
 }
 
 export interface FunctionClause {
-    function: Function;
-    argument: Argument[];
+  function: Function;
+  argument: Argument[];
 }
 
 export interface Match extends Element, Whereable {
-    exclude: boolean;
-    boolMatch: Bool;
-    description: string;
-    graph: Element;
-    path: Element[];
-    match: Match[];
-    orderBy: OrderLimit[];
+  exclude: boolean;
+  boolMatch: Bool;
+  description: string;
+  graph: Element;
+  path: Element[];
+  match: Match[];
+  orderBy: OrderLimit[];
 }
 
 export interface OrderLimit extends Property {
-    direction: Order;
-    limit: number;
-    property: string;
+  direction: Order;
+  limit: number;
+  property: string;
 }
 
 export interface PathDocument {
-    match: Match[];
+  match: Match[];
 }
 
 export interface PathQuery extends TTIriRef {
-    source: TTIriRef;
-    target: TTIriRef;
-    depth: number;
+  source: TTIriRef;
+  target: TTIriRef;
+  depth: number;
 }
 
 export interface Property extends Element {
-    node: string;
+  node: string;
 }
 
 export interface Query extends TTIriRef {
-    match: Match[];
-    select: Select[];
-    groupBy: Element[];
-    orderBy: OrderLimit[];
-    activeOnly: boolean;
-    usePrefixes: boolean;
-    query: Query[];
+  match: Match[];
+  select: Select[];
+  groupBy: Element[];
+  orderBy: OrderLimit[];
+  activeOnly: boolean;
+  usePrefixes: boolean;
+  query: Query[];
 }
 
 export interface QueryEntity extends Entity {
-    definition: Query;
+  definition: Query;
 }
 
 export interface QueryRequest {
-    name: string;
-    page: Page;
-    context: TTContext;
-    textSearch: string;
-    argument: Argument[];
-    query: Query;
-    pathQuery: PathQuery;
-    update: Update;
-    referenceDate: string;
+  name: string;
+  page: Page;
+  context: TTContext;
+  textSearch: string;
+  argument: Argument[];
+  query: Query;
+  pathQuery: PathQuery;
+  update: Update;
+  referenceDate: string;
 }
 
 export interface Range {
-    to: Assignable;
-    from: Assignable;
+  to: Assignable;
+  from: Assignable;
 }
 
 export interface Select extends Property {
-    case: Case[];
-    select: Select[];
-    function: FunctionClause;
+  case: Case[];
+  select: Select[];
+  function: FunctionClause;
 }
 
 export interface Update extends TTIriRef {
-    match: Match;
-    delete: Delete[];
+  match: Match;
+  delete: Delete[];
 }
 
-export interface Value extends Assignable {
-}
+export interface Value extends Assignable {}
 
 export interface Where extends Property, Assignable, Whereable {
-    description: string;
-    range: Range;
-    in: Element[];
-    notIn: Element[];
-    anyRoleGroup: boolean;
-    valueLabel: string;
-    null: boolean;
+  description: string;
+  range: Range;
+  in: Element[];
+  notIn: Element[];
+  anyRoleGroup: boolean;
+  valueLabel: string;
+  null: boolean;
 }
 
 export interface Whereable {
-    where: Where[];
-    bool: Bool;
+  where: Where[];
+  bool: Bool;
 }
 
 export interface EntityDocument {
-    id: number;
-    iri: string;
-    name: string;
-    length: number;
-    preferredName: string;
-    code: string;
-    matchTerm: string[];
-    key: string[];
-    scheme: TTIriRef;
-    entityType: TTIriRef[];
-    status: TTIriRef;
-    termCode: SearchTermCode[];
-    weighting: number;
-    match: string;
-    isA: TTIriRef[];
-    isDescendentOf: TTIriRef[];
+  id: number;
+  iri: string;
+  name: string;
+  length: number;
+  preferredName: string;
+  code: string;
+  matchTerm: string[];
+  key: string[];
+  scheme: TTIriRef;
+  entityType: TTIriRef[];
+  status: TTIriRef;
+  termCode: SearchTermCode[];
+  weighting: number;
+  match: string;
+  isA: TTIriRef[];
+  isDescendentOf: TTIriRef[];
 }
 
 export interface SearchResultSummary {
-    name: string;
-    iri: string;
-    code: string;
-    description: string;
-    status: TTIriRef;
-    scheme: TTIriRef;
-    entityType: TTIriRef[];
-    weighting: number;
-    match: string;
-    preferredName: string;
-    key: string[];
-    isA: TTIriRef[];
-    termCode: SearchTermCode[];
+  name: string;
+  iri: string;
+  code: string;
+  description: string;
+  status: TTIriRef;
+  scheme: TTIriRef;
+  entityType: TTIriRef[];
+  weighting: number;
+  match: string;
+  preferredName: string;
+  key: string[];
+  isA: TTIriRef[];
+  termCode: SearchTermCode[];
 }
 
 export interface SearchTermCode {
-    term: string;
-    code: string;
-    status: TTIriRef;
+  term: string;
+  code: string;
+  status: TTIriRef;
 }
 
 export interface TTIriRef extends TTValue, Serializable {
-    name: string;
-    description: string;
-    "@id": string;
+  name: string;
+  description: string;
+  "@id": string;
 }
 
 export interface TTContext extends Serializable {
-    prefixes: TTPrefix[];
-    nameSpaces: TTPrefix[];
+  prefixes: TTPrefix[];
+  nameSpaces: TTPrefix[];
 }
 
 export interface TTTypedRef extends TTIriRef {
-    type: TTIriRef;
+  type: TTIriRef;
 }
 
 export interface TTValue extends Serializable {
-    order: number;
+  order: number;
 }
 
-export interface Serializable {
-}
+export interface Serializable {}
 
 export interface TTPrefix {
-    iri: string;
-    prefix: string;
-    name: string;
+  iri: string;
+  prefix: string;
+  name: string;
 }
 
 export type ListMode = "ALL" | "FIRST" | "REST";
