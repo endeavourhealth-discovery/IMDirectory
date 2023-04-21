8ca4524aac6750d276b3efe7ec521f9ed00d4aa1
matt-voror <matt@voror.co.uk>
2023-04-21 15:26:56 +0100

    Added generic setup for tests, including blocking access to real fetch requests. Fixed bug with typescript in tests.

---
+++ node_api/tests/setupTests.ts
@@ -0,0 +1,17 @@
+import { beforeAll, afterAll, afterEach, it, expect, beforeEach, describe } from "vitest";
+import { setupServer } from "msw/node";
+
+const restHandlers = [];
+const server = setupServer(...restHandlers);
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterAll(() => {
+  server.close();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
