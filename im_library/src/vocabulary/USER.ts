c70561d9c0255f8bf425d62ebfd75741cee3b0be
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2023-05-16 13:19:12 +0100

    user vocab file added for database

---
+++ im_library/src/vocabulary/USER.ts
@@ -0,0 +1,9 @@
+export class USER {
+  public static DOMAIN = "http://endhealth.info/";
+  public static NAMESPACE = USER.DOMAIN + "user#";
+  public static PREFIX = "usr";
+
+  // USER entries
+  public static USER_THEME = USER.NAMESPACE + "UserTheme";
+  public static USER_MRU = USER.NAMESPACE + "UserMRU";
+}
