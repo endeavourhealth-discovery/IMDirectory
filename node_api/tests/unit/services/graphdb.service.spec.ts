463b613ff97bef71cdd3331494bcfb7c37bfdc1b
Richard Collier <gitkraken@tirinoarim.co.uk>
2023-05-26 16:38:35 +0100

    Fix unit test data

--- node_api/tests/unit/services/graphdb.service.spec.ts
+++ node_api/tests/unit/services/graphdb.service.spec.ts
@@ -6,7 +6,7 @@ describe("graphdb service", () => {
     it("can desanitise", () => {
       expect(
         desanitise(
-          '"{`version`:`v2.4.0`,`title`:`v2.4.0 Release`,`createdDate`:`30/2/2023 15:39:11`,`publishedDate`:`30/2/2023 16:49:7`,`releaseNotes`:[`- Numerous visual improvements`,`- Icon updates (Font Awesome Pro)`,`- Themes`,`- Search optimisations`,`- ECL builder extensions`,`- Data model viewer enhancements`,`- Java code generation`,`- Latest SNOMED`,`- Query display enhancements`,`- Improved error handling & reporting`],`author`:`VororRich`,`url`:`https://github.com/endeavourhealth-discovery/IMDirectory/releases/tag/v2.4.0`}"'
+          '{`version`:`v2.4.0`,`title`:`v2.4.0 Release`,`createdDate`:`30/2/2023 15:39:11`,`publishedDate`:`30/2/2023 16:49:7`,`releaseNotes`:[`- Numerous visual improvements`,`- Icon updates (Font Awesome Pro)`,`- Themes`,`- Search optimisations`,`- ECL builder extensions`,`- Data model viewer enhancements`,`- Java code generation`,`- Latest SNOMED`,`- Query display enhancements`,`- Improved error handling & reporting`],`author`:`VororRich`,`url`:`https://github.com/endeavourhealth-discovery/IMDirectory/releases/tag/v2.4.0`}'
         )
       ).toEqual({
         version: "v2.4.0",
