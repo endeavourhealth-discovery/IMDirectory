4456047d44c6d70e263fb2565e7d21b655602ac6
Kostas Kardatos <kon.kardatos@gmail.com>
2023-05-26 11:16:43 +0300

    Viewer does not set up config properly Fixes #765

--- ui/src/composables/setupConfig.ts
+++ ui/src/composables/setupConfig.ts
@@ -1,4 +1,4 @@
-import { DefinitionConfig } from "@im-library/interfaces";
+import { DashboardLayout, DefinitionConfig } from "@im-library/interfaces";
 import { ref, Ref } from "vue";
 import { ConfigService } from "@/services";
 import { isObjectHasKeys } from "@im-library/helpers/DataTypeCheckers";
@@ -11,15 +11,27 @@ function setupConfig() {
   const configs: Ref<DefinitionConfig[]> = ref([]);
 
   async function getConfig(): Promise<void> {
-    const definitionConfig = await ConfigService.getComponentLayout("definition");
-    const summaryConfig = await ConfigService.getComponentLayout("summary");
-    configs.value = definitionConfig.concat(summaryConfig);
+    const definitionConfigString = await ConfigService.getComponentLayout("definition");
+    const definitionConfig: DefinitionConfig[] = parseConfig(definitionConfigString);
+    const summaryConfigString = await ConfigService.getComponentLayout("summary");
+    const summaryConfig: DashboardLayout[] = parseConfig(summaryConfigString);
+
+    configs.value = definitionConfig.concat(summaryConfig as any[]) as DefinitionConfig[];
 
     if (configs.value.every(config => isObjectHasKeys(config, ["order"]))) {
       configs.value.sort(byOrder);
     } else {
       log.error("Failed to sort config for definition component layout. One or more config items are missing 'order' property.");
     }
+  }
+
+  function parseConfig(configString: string) {
+    if (!configString) {
+      return [];
+    }
+    const procString = configString.slice(1, -1);
+    const parsed = JSON.parse(procString);
+    return parsed as any[];
   }
 
   return { configs, getConfig };
