571e52a9302ae2f2e3269c635146b017b601df48
ZeynepBayazitoglu <zeynep@voror.co.uk>
2023-05-31 12:16:19 +0100

    Added form generator for value set

--- im_library/src/vocabulary/im/editorVocabulary.ts
+++ im_library/src/vocabulary/im/editorVocabulary.ts
@@ -3,6 +3,7 @@ export class editorVocabulary {
 
   public static CONCEPT_SHAPE = this.NAMESPACE + "ConceptShape";
   public static CONCEPT_SET_SHAPE = this.NAMESPACE + "ConceptSetShape";
+  public static VALUE_SET_SHAPE = this.NAMESPACE + "ValueSetShape";
   public static FOLDER_SHAPE = this.NAMESPACE + "FolderShape";
   public static DATA_MODEL_SHAPE = this.NAMESPACE + "DataModelShape";
   public static QUERY_SHAPE = this.NAMESPACE + "QueryShape";
