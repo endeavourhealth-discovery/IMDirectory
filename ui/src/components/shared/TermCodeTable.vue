69fa245c3b9885f37842c798bc8d7b4d4868b385
matt-voror <matt@voror.co.uk>
2023-09-13 14:47:56 +0100

    name reverted to term

--- ui/src/components/shared/TermCodeTable.vue
+++ ui/src/components/shared/TermCodeTable.vue
@@ -2,7 +2,7 @@
   <div class="termcode-table-container">
     <DataTable class="p-datatable-sm term-code-table scrollbar" :value="terms" responsiveLayout="scroll" scrollHeight="flex">
       <template #empty> No terms found. </template>
-      <Column field="name" header="Name"> </Column>
+      <Column field="term" header="Term"> </Column>
       <Column field="code" header="Code"></Column>
       <Column field="status.name" header="Status"></Column>
     </DataTable>
@@ -11,10 +11,9 @@
 
 <script setup lang="ts">
 import { TTIriRef } from "@im-library/interfaces/AutoGen";
-import { PropType } from "vue";
 
 interface Props {
-  terms: { name: string; code: string; status: TTIriRef }[];
+  terms: { term: string; code: string; status: TTIriRef }[];
 }
 
 const props = defineProps<Props>();
