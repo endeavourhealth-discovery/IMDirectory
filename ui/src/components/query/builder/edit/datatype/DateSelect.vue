c1824baf07d6a689a22c4fedc2f6c0f16e4a9550
Kostas Kardatos <kon.kardatos@gmail.com>
2023-09-14 17:08:09 +0300

    Implement date property after variable WIP Fixes #1183

--- ui/src/components/query/builder/edit/datatype/DateSelect.vue
+++ ui/src/components/query/builder/edit/datatype/DateSelect.vue
@@ -7,7 +7,18 @@
   </div>
   <div v-else-if="propertyType === 'is'">
     <Dropdown type="text" placeholder="operator" :options="operatorOptions" v-model="property.operator" />
-    <Calendar v-model:model-value="selectedValueA" dateFormat="dd/mm/yy" />
+    <Dropdown type="text" placeholder="value type" :options="['date', 'variable']" v-model="valueType" />
+    <Calendar v-if="valueType === 'date'" v-model:model-value="selectedValueA" dateFormat="dd/mm/yy" />
+    <TreeSelect
+      v-else-if="valueType === 'variable'"
+      v-model="selectedValueA"
+      :options="variableOptions"
+      :filter="true"
+      filterMode="strict"
+      placeholder="Select Item"
+      class="md:w-20rem w-full"
+      selection-mode="single"
+    />
   </div>
   <div v-else-if="propertyType === 'within'">
     the last <InputNumber v-model:model-value="numberValue" />
@@ -17,34 +28,35 @@
 </template>
 
 <script setup lang="ts">
+import { EntityService } from "@/services";
+import { useQueryStore } from "@/stores/queryStore";
 import { isObjectHasKeys } from "@im-library/helpers/DataTypeCheckers";
-import { Property, Range } from "@im-library/interfaces/AutoGen";
+import { getNameFromRef } from "@im-library/helpers/TTTransform";
+import { Property, PropertyRef, Range } from "@im-library/interfaces/AutoGen";
+import { SHACL } from "@im-library/vocabulary";
 import { cloneDeep } from "lodash";
-import { Ref, onMounted, ref, watch } from "vue";
+import { TreeNode } from "primevue/tree";
+import { ComputedRef, Ref, computed, onMounted, ref, watch } from "vue";
+
 interface Props {
   property: Property;
+  datatype: string;
 }
 const props = defineProps<Props>();
+const queryStore = useQueryStore();
+
+const variableMap: ComputedRef<Map<string, any>> = computed(() => queryStore.$state.variableMap);
+
 const propertyType: Ref<"is" | "between" | "within"> = ref("is");
+const valueType: Ref<"date" | "variable"> = ref("date");
 const selectedValueA: Ref<any> = ref();
 const selectedValueB: Ref<any> = ref();
 const operatorOptions = ["=", ">=", ">", "<="];
-const numberValue = ref(0);
+const numberValue: Ref<number> = ref(0);
+const variableOptions: Ref<TreeNode[]> = ref([]);
 
-onMounted(() => {
-  if (props.property.value) {
-    if (isNumber(props.property.value)) {
-      propertyType.value = "within";
-      numberValue.value = ~~props.property.value;
-    } else if (props.property.operator === "=") {
-      propertyType.value = "is";
-      selectedValueA.value = getDateFromString(props.property.value);
-    }
-  } else if (isObjectHasKeys(props.property, ["range"])) {
-    propertyType.value = "between";
-    if (props.property.range?.from.value) selectedValueA.value = getDateFromString(props.property.range!.from.value);
-    if (props.property.range?.to.value) selectedValueB.value = getDateFromString(props.property.range!.to.value);
-  }
+onMounted(async () => {
+  await initValues();
 });
 
 watch(
@@ -64,6 +76,99 @@ watch(
   () => updatePropertyValues()
 );
 
+async function initValues() {
+  variableOptions.value = await getVariableOptions();
+  if (props.property.value) {
+    if (isNumber(props.property.value)) {
+      propertyType.value = "within";
+      numberValue.value = ~~props.property.value;
+    } else {
+      propertyType.value = "is";
+      selectedValueA.value = getDateFromString(props.property.value);
+    }
+  } else if (isObjectHasKeys(props.property, ["range"])) {
+    propertyType.value = "between";
+    if (props.property.range?.from.value) selectedValueA.value = getDateFromString(props.property.range!.from.value);
+    if (props.property.range?.to.value) selectedValueB.value = getDateFromString(props.property.range!.to.value);
+  } else if (props.property.relativeTo) {
+    propertyType.value = "is";
+    valueType.value = "variable";
+    const key = getKeyFromRelativeTo(props.property.relativeTo);
+    if (key) {
+      selectedValueA.value = {};
+      selectedValueA.value[key] = true;
+    }
+  }
+}
+
+function findNodeByKey(nodes: TreeNode[], key?: string) {
+  if (!key) return undefined;
+
+  let found = nodes.find(node => node.key === key);
+  if (found) return found;
+  for (const node of nodes) {
+    const nestedFound = node.children?.find(node => node.key === key);
+    if (nestedFound) found = nestedFound;
+  }
+  return found;
+}
+
+function getKeyFromRelativeTo(relativeTo: PropertyRef) {
+  if (!relativeTo.nodeRef) return relativeTo["@id"];
+  return relativeTo.nodeRef + "/" + relativeTo["@id"];
+}
+
+async function getVariableOptions() {
+  // TODO: Add root/base type data model properties
+  const options: TreeNode[] = [];
+  for (const [key, value] of variableMap.value.entries()) {
+    const dataModelIri = getVariableWithType(value);
+    const properties = await getValidProperties(dataModelIri);
+    const treeNode = {
+      key: key,
+      label: key + " (" + getNameFromRef({ "@id": dataModelIri }) + ")",
+      children: getTreeNodesFromProperties(key, properties),
+      selectable: false
+    } as TreeNode;
+
+    options.push(treeNode);
+  }
+  return options;
+}
+
+function getTreeNodesFromProperties(key: string, properties: any[]) {
+  const treeNodes: TreeNode[] = [];
+  for (const property of properties) {
+    treeNodes.push({
+      key: key + "/" + property[SHACL.PATH][0]["@id"],
+      label: property[SHACL.PATH][0].name,
+      data: {
+        "@id": property[SHACL.PATH][0]["@id"],
+        nodeRef: key,
+        name: property[SHACL.PATH][0].name
+      }
+    });
+  }
+  return treeNodes;
+}
+
+async function getValidProperties(dataModelIri: string) {
+  const propertiesEntity = await EntityService.getPartialEntity(dataModelIri, [SHACL.PROPERTY]);
+  if (!isObjectHasKeys(propertiesEntity, [SHACL.PROPERTY])) return [];
+
+  const allProperties: any[] = propertiesEntity[SHACL.PROPERTY];
+  return allProperties.filter(dmProperty => dmProperty[SHACL.DATATYPE] && dmProperty[SHACL.DATATYPE][0]["@id"] === props.datatype);
+}
+
+function getVariableWithType(value: any) {
+  if (isObjectHasKeys(value, ["typeOf"])) return value.typeOf["@id"];
+  else if (isObjectHasKeys(value, ["nodeRef"])) {
+    const objectWithTypeOf = variableMap.value.get(value.nodeRef);
+    if (isObjectHasKeys(objectWithTypeOf, ["typeOf"])) return objectWithTypeOf.typeOf["@id"];
+  }
+  // return props.datatype
+}
+
 function isNumber(stringNumber: string) {
   return /^-?\d+$/.test(stringNumber);
 }
@@ -71,7 +176,8 @@ function isNumber(stringNumber: string) {
 function updatePropertyValues() {
   if (propertyType.value === "is") {
     delete props.property.range;
-    if (selectedValueA.value) props.property.value = getStringFromDate(selectedValueA.value);
+    if (selectedValueA.value && valueType.value === "date") props.property.value = getStringFromDate(selectedValueA.value);
+    else if (selectedValueA.value && valueType.value === "variable") props.property.relativeTo = geRelativeToValue();
   } else if (propertyType.value === "between") {
     delete props.property.operator;
     delete props.property.value;
@@ -84,6 +190,11 @@ function updatePropertyValues() {
   }
 }
 
+function geRelativeToValue() {
+  const found = findNodeByKey(variableOptions.value, Object.keys(selectedValueA.value)[0]);
+  if (found) return found.data;
+}
+
 function getStringFromDate(date: Date) {
   return [date.getDate(), date.getMonth() + 1, date.getFullYear()].join("/");
 }
@@ -91,7 +202,7 @@ function getStringFromDate(date: Date) {
 function getDateFromString(date: string) {
   let separator = "";
   if (date.includes("-")) separator = "-";
-  if (date.includes("/")) separator = "/";
+  else if (date.includes("/")) separator = "/";
   const splits = date.split(separator);
   if (splits.length !== 3) return new Date();
 
