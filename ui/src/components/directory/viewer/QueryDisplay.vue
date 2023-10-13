b24f7ef3eb013ff0b202e0167d7c926c3f4b6f2d
Kostas Kardatos <kon.kardatos@gmail.com>
2023-10-13 14:45:13 +0300

    show test query only if user has edit/create role

--- ui/src/components/directory/viewer/QueryDisplay.vue
+++ ui/src/components/directory/viewer/QueryDisplay.vue
@@ -2,7 +2,7 @@
   <div class="query-display-container flex flex-column gap-3">
     <div class="flex flex-row gap-2">
       <div><Button label="Generate SQL" @click="generateSQL" data-testid="sql-button" /></div>
-      <QuickQuery :query-iri="entityIri">
+      <QuickQuery :query-iri="entityIri" v-if="canTestQuery">
         <template #button="{ runQuickQuery }">
           <Button icon="pi pi-bolt" label="Test query" severity="help" @click="runQuickQuery" class="quick-query-button" />
         </template>
@@ -29,20 +29,26 @@ import RecursiveQueryDisplay from "@/components/query/viewer/RecursiveQueryDispl
 import { QueryService } from "@/services";
 import { isArrayHasLength } from "@im-library/helpers/DataTypeCheckers";
 import { Query } from "@im-library/interfaces/AutoGen";
-import { onMounted, watch, Ref, ref } from "vue";
+import { onMounted, watch, Ref, ref, computed } from "vue";
 import { useToast } from "primevue/usetoast";
 import { ToastOptions } from "@im-library/models";
 import { ToastSeverity } from "@im-library/enums";
 import QuickQuery from "@/components/query/QuickQuery.vue";
+import { useUserStore } from "@/stores/userStore";
 
 interface Props {
   entityIri: string;
 }
+
+const userStore = useUserStore();
 const props = defineProps<Props>();
 const query: Ref<Query> = ref({} as Query);
 const sql: Ref<string> = ref("");
 const showSql: Ref<boolean> = ref(false);
 const toast = useToast();
+const canTestQuery = computed(
+  () => userStore.isLoggedIn && (userStore.currentUser?.roles?.includes("create") || userStore.currentUser?.roles?.includes("edit"))
+);
 
 watch(
   () => props.entityIri,
