1fcae9a7665a0a610c3141c4a11e9ddc0a3b13b2
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2023-10-16 14:17:21 +0100

    added validation for status and updated concept shape to include it

--- im_library/src/vocabulary/im/validationVocabulary.ts
+++ im_library/src/vocabulary/im/validationVocabulary.ts
@@ -7,4 +7,5 @@ export class validationVocabulary {
   public static IS_TERMCODE = this.NAMESPACE + "isTermcode";
   public static IS_PROPERTY = this.NAMESPACE + "isProperty";
   public static IS_SCHEME = this.NAMESPACE + "isScheme";
+  public static IS_STATUS = this.NAMESPACE + "isStatus";
 }
