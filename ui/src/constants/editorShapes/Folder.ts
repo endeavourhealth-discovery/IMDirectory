c3e5d079ab65a20433ccdc86af8841de8d56a747
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2023-10-16 14:21:11 +0100

    added status validation to folder shape

--- ui/src/constants/editorShapes/Folder.ts
+++ ui/src/constants/editorShapes/Folder.ts
@@ -157,6 +157,7 @@ const FolderShape: FormGenerator = {
               order: 6,
               path: { "@id": IM.HAS_STATUS },
               componentType: { "@id": IM.component.ARRAY_BUILDER },
+              validation: { "@id": IM.validation.IS_STATUS },
               minCount: 1,
               arrayButtons: { up: false, down: false, plus: false, minus: false },
               property: [
