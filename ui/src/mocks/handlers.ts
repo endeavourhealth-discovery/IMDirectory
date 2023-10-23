6d561a724f04b8058c2131a7e09eaef4d8e54e2b
Kostas Kardatos <kon.kardatos@gmail.com>
2023-10-23 12:47:44 +0100

    update msw to version 2.0.0

--- ui/src/mocks/handlers.ts
+++ ui/src/mocks/handlers.ts
@@ -1,60 +1,105 @@
-import { rest } from "msw";
+import { http } from "msw";
 import { IM } from "@im-library/vocabulary";
 import { fakerFactory } from "@im-library/mocks/fakerFactory";
 
 const apiUrl = "http://localhost:8082/imapi/api/";
 
 export const handlers = [
-  rest.get(apiUrl + "entity/public/children", async (req, res, ctx) => {
-    const iri = req.url.searchParams.get("iri");
-    if (iri === IM.STATUS) return res(ctx.status(200), ctx.json([IM.ACTIVE, IM.DRAFT, IM.INACTIVE]));
-    else
-      return res(
-        ctx.status(500),
-        ctx.json({
+  http.get(apiUrl + "entity/public/children", async ({ request }) => {
+    const url = new URL(request.url);
+    const iri = url.searchParams.get("iri");
+    if (iri === IM.STATUS) {
+      return new Response(JSON.stringify([IM.ACTIVE, IM.DRAFT, IM.INACTIVE]), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        status: 200
+      });
+    } else {
+      return new Response(
+        JSON.stringify({
           errorMessage: "Fetch mocking is active for this api url. Please mock the fetch request for this url parameter/body in src/mocks/handler.js"
-        })
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 500
+        }
       );
+    }
   }),
-  rest.post(apiUrl + "query/public/entityQuery", async (req, res, ctx) => {
-    const body = await req.json();
-    if (body.queryIri["@id"] === "http://endhealth.info/im#Query_GetIsas") {
-      if (body.argument.this === "http://endhealth.info/im#Status")
-        return res(
-          ctx.status(200),
-          ctx.json([
+  http.post(apiUrl + "query/public/entityQuery", async ({ request }) => {
+    const body: any = await request.json();
+    const { queryIri, argument } = body;
+    if (queryIri["@id"] === "http://endhealth.info/im#Query_GetIsas") {
+      if (argument.this === "http://endhealth.info/im#Status") {
+        return new Response(
+          JSON.stringify([
             { "@id": IM.ACTIVE, "http://www.w3.org/2000/01/rdf-schema#label": "Active" },
             { "@id": IM.DRAFT, "http://www.w3.org/2000/01/rdf-schema#label": "Draft" },
             { "@id": IM.INACTIVE, "http://www.w3.org/2000/01/rdf-schema#label": "Inactive" }
-          ])
+          ]),
+          {
+            headers: {
+              "Content-Type": "application/json"
+            },
+            status: 500
+          }
         );
-      else return res(ctx.status(200), ctx.json([]));
-    } else
-      return res(
-        ctx.status(500),
-        ctx.json({
+      } else {
+        return new Response(JSON.stringify([]), {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 200
+        });
+      }
+    } else {
+      return new Response(
+        JSON.stringify({
           errorMessage: "Fetch mocking is active for this api url. Please mock the fetch request for this url parameter/body in src/mocks/handler.js"
-        })
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 500
+        }
       );
+    }
   }),
-  rest.post(apiUrl + "function/public/callFunction", async (req, res, ctx) => {
-    const body = await req.json();
-    if (body.functionIri === "http://endhealth.info/im#GetAdditionalAllowableTypes") return res(ctx.status(200), ctx.json([]));
-    else
-      return res(
-        ctx.status(500),
-        ctx.json({
+  http.post(apiUrl + "function/public/callFunction", async ({ request }) => {
+    const body: any = await request.json();
+    if (body.functionIri === "http://endhealth.info/im#GetAdditionalAllowableTypes") {
+      return new Response(JSON.stringify([]), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        status: 200
+      });
+    } else {
+      return new Response(
+        JSON.stringify({
           errorMessage: "Fetch mocking is active for this api url. Please mock the fetch request for this url parameter/body in src/mocks/handler.js"
-        })
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 500
+        }
       );
+    }
   })
 ];
 
 export const handlersFaker = [
-  rest.get(apiUrl + "entity/public/partial", async (req, res, ctx) => {
+  http.get(apiUrl + "entity/public/partial", async ({ request }) => {
     console.log("using msw");
-    const iri = req.url.searchParams.get("iri");
-    const predicates = req.url.searchParams.get("predicates");
+    const url = new URL(request.url);
+    const iri = url.searchParams.get("iri");
+    const predicates = url.searchParams.get("predicates");
     const predicatesArray = predicates?.split(",");
     const entityValue = {} as any;
     if (iri) entityValue["@id"] = iri;
@@ -65,28 +110,66 @@ export const handlersFaker = [
     Object.keys(entity).forEach((key: string) => {
       if (!entity[key]) delete entity[key];
     });
-    return res(ctx.status(200), ctx.json(entity));
+    return new Response(JSON.stringify(entity), {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      status: 200
+    });
   }),
-  rest.get(apiUrl + "entity/public/parents", async (req, res, ctx) => {
-    const iri = req.url.searchParams.get("iri");
-    return res(ctx.status(200), ctx.json([fakerFactory.entitySummary.create(), fakerFactory.entitySummary.create()]));
+  http.get(apiUrl + "entity/public/parents", async ({ request }) => {
+    const url = new URL(request.url);
+    const iri = url.searchParams.get("iri");
+    return new Response(JSON.stringify([fakerFactory.entitySummary.create(), fakerFactory.entitySummary.create()]), {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      status: 200
+    });
   }),
-  rest.get(apiUrl + "entity/public/childrenPaged", async (req, res, ctx) => {
-    const iri = req.url.searchParams.get("iri");
+  http.get(apiUrl + "entity/public/childrenPaged", async ({ request }) => {
+    const url = new URL(request.url);
+    const iri = url.searchParams.get("iri");
     const children = [
       fakerFactory.entitySummary.create(),
       fakerFactory.entitySummary.create(),
       fakerFactory.entitySummary.create(),
       fakerFactory.entitySummary.create()
     ];
-    return res(ctx.status(200), ctx.json(fakerFactory.pagedChildren.create({ result: children, totalCount: 4 })));
+    return new Response(JSON.stringify(fakerFactory.pagedChildren.create({ result: children, totalCount: 4 })), {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      status: 200
+    });
   }),
-  rest.get(apiUrl + "entity/public/summary", async (req, res, ctx) => {
-    const iri = req.url.searchParams.get("iri");
+  http.get(apiUrl + "entity/public/summary", async ({ request }) => {
+    const url = new URL(request.url);
+    const iri = url.searchParams.get("iri");
     if (iri) {
       const found = fakerFactory.entitySummary.findFirst({ where: { "@id": { equals: iri } } });
-      if (found) return res(ctx.status(200), ctx.json(found));
-      else return res(ctx.status(200), ctx.json(fakerFactory.pagedChildren.create()));
-    } else return res(ctx.status(500), ctx.json({ errorMessage: "Missing iri parameter" }));
+      if (found) {
+        return new Response(JSON.stringify(found), {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 200
+        });
+      } else {
+        return new Response(JSON.stringify(fakerFactory.pagedChildren.create()), {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          status: 200
+        });
+      }
+    } else {
+      return new Response(JSON.stringify({ errorMessage: "Missing iri parameter" }), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        status: 500
+      });
+    }
   })
 ];
