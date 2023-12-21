6f3ed56135dc47ce29afac8a0a035fec0a09fcf6
VororJo <122791902+VororJo@users.noreply.github.com>
2023-12-21 16:54:07 +0000

    deleted incorrectly added files

--- im_library/src/vocabulary/SNOMED.ts
+++
@@ -1,6 +0,0 @@
-export class SNOMED {
-	public static readonly DOMAIN = "http://snomed.info/";
-	public static readonly NAMESPACE = SNOMED.DOMAIN + "sct#";
-	public static readonly PREFIX = "sn";
-	public static readonly ATTRIBUTE = SNOMED.DOMAIN + "246061005";
-}
