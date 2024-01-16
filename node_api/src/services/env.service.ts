663f8a5e36a24a970e7b9badabf2ba6e7243ed37
matt-voror <matt@voror.co.uk>
2024-01-16 14:56:50 +0000

    env fixes

--- node_api/src/services/env.service.ts
+++ node_api/src/services/env.service.ts
@@ -1,6 +1,6 @@
 const Env = {
   API: process.env.VITE_API ?? "http://localhost:8080/",
-  GIT_TOKEN: import.meta.env.VITE_GIT_TOKEN ?? null,
+  GIT_TOKEN: process.env.VITE_GIT_TOKEN ?? null,
   GRAPH_HOST: process.env.GRAPH_HOST ?? "http://localhost:7200",
   GRAPH_REPO: process.env.GRAPH_REPO ?? "im",
   GRAPH_REPO_CONFIG: process.env.GRAPH_REPO_CONFIG ?? "config",
@@ -10,7 +10,7 @@ const Env = {
   OPENSEARCH_URL: process.env.OPENSEARCH_URL ?? "https://search.endeavourhealth.net/concept/_search",
   OPENSEARCH_AUTH: process.env.OPENSEARCH_AUTH ?? "",
   COGNITO_REGION: process.env.COGNITO_REGION ?? "eu-west-2",
-  COGNITO_USER_POOL: import.meta.env.VITE_COGNITO_USER_POOL ?? "",
+  COGNITO_USER_POOL: process.env.VITE_COGNITO_USER_POOL ?? "",
   MYSQL_HOST: process.env.MYSQL_HOST ?? "localhost",
   MYSQL_USER: process.env.MYSQL_USER ?? "root",
   MYSQL_PASS: process.env.MYSQL_PASS ?? "password",
