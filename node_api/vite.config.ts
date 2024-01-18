82028cbc1a621411d867257096191aecf90719b7
matt-voror <matt@voror.co.uk>
2024-01-18 16:09:40 +0000

    inject env values to node_api

--- node_api/vite.config.ts
+++ node_api/vite.config.ts
@@ -1,33 +1,39 @@
 import { defineConfig } from "vitest/config";
 import { VitePluginNode } from "vite-plugin-node";
 import * as path from "path";
+import { loadEnv } from "vite";
 
-export default defineConfig({
-  server: {
-    port: 3000
-  },
-  plugins: [
-    ...VitePluginNode({
-      adapter: "express",
-      appPath: "./src/server.ts",
-      exportName: "viteNodeApp",
-      tsCompiler: "esbuild",
-      swcOptions: {}
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@im-library": path.resolve(__dirname, "./../im_library/src"),
-      "./runtimeConfig": "./runtimeConfig.browser"
-    }
-  },
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    coverage: {
-      reporter: ["text", "lcov"]
+export default ({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    define: { "process.env": env },
+    server: {
+      port: 3000
     },
-    setupFiles: "./tests/setupTests.js"
-  }
-});
+    plugins: [
+      ...VitePluginNode({
+        adapter: "express",
+        appPath: "./src/server.ts",
+        exportName: "viteNodeApp",
+        tsCompiler: "esbuild",
+        swcOptions: {}
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@im-library": path.resolve(__dirname, "./../im_library/src"),
+        "./runtimeConfig": "./runtimeConfig.browser"
+      }
+    },
+    test: {
+      globals: true,
+      environment: "happy-dom",
+      coverage: {
+        reporter: ["text", "lcov"]
+      },
+      setupFiles: "./tests/setupTests.js"
+    }
+  });
+};
