55f9a0b2b0a75b7c1a6923a57d400217506d6a54
Richard Collier <rich@voror.co.uk>
2024-03-06 15:02:12 +0000

    Implemented UI scaling

--- im_library/src/vocabulary/USER.ts
+++ im_library/src/vocabulary/USER.ts
@@ -5,6 +5,7 @@ export class USER {
   public static readonly NAMESPACE = USER.DOMAIN + "user#";
   public static readonly PREFIX = "usr";
   public static readonly USER_THEME = USER.DOMAIN + "UserTheme";
+  public static readonly USER_SCALE = USER.DOMAIN + "UserScale";
   public static readonly USER_MRU = USER.DOMAIN + "UserMRU";
   public static readonly USER_FAVOURITES = USER.DOMAIN + "UserFavourites";
   public static readonly ORGANISATIONS = USER.DOMAIN + "ORGANISATIONS";
