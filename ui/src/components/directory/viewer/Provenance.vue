05bb83615505d5057ebf6c71987de94a69b1f41e
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2024-03-27 16:38:55 +0000

    added provenance dialog on click

--- ui/src/components/directory/viewer/Provenance.vue
+++ ui/src/components/directory/viewer/Provenance.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="provenance-container">
-    <DataTable :value="provenances" show-gridlines :scrollable="true" :sortOrder="1" class="p-datatable-sm" scrollHeight="flex" :loading="loading">
+    <DataTable
+      @row-click="onRowClick()"
+      :value="provenances"
+      show-gridlines
+      :scrollable="true"
+      :sortOrder="1"
+      class="p-datatable-sm"
+      scrollHeight="flex"
+      :loading="loading"
+      :style="'cursor:pointer;'"
+      selectionMode="single"
+      v-model:selection="selectedProvenance"
+    >
       <Column field="prov" header="Provenance Id" style="width: 18rem"></Column>
       <Column field="usedEntity" header="Used Entity" style="min-width: 18rem"></Column>
       <Column field="effectiveDate" header="Effective Date" style="min-width: 18rem"></Column>
       <Column field="activityType" header="Activity Type" style="max-width: 10rem"></Column>
       <Column field="agent" header="Agent" style="max-width: 10rem"></Column>
     </DataTable>
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Test!"
+      :style="{
+        minWidth: '90vw',
+        minHeight: '90vh',
+        display: 'flex',
+        flexFlow: 'column nowrap'
+      }"
+      @hide="onClose()"
+    >
+      <Panel header="JSON" toggleable>
+        <JSONViewer :entity-iri="jsonDisplay"></JSONViewer>
+      </Panel>
+    </Dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ProvService } from "@/services";
-import { IM } from "@im-library/vocabulary";
-import { onMounted, ref, Ref } from "vue";
+import { EntityService, ProvService } from "@/services";
+import { IM, RDFS } from "@im-library/vocabulary";
+import { onMounted, ref, Ref, watch } from "vue";
+import JSONViewer from "@/components/directory/viewer/JSONViewer.vue";
+import { isObjectHasKeys } from "@im-library/helpers/DataTypeCheckers";
 
 interface Props {
   entityIri: string;
@@ -22,8 +52,25 @@ const props = defineProps<Props>();
 
 const provenances: Ref<any[]> = ref([]);
 const loading: Ref<boolean> = ref(false);
+const visible = ref(false);
+const selectedProvenance = ref();
+const jsonDisplay = ref();
 
 onMounted(async () => await getProvHistory(props.entityIri));
+
+watch(selectedProvenance, async () => {
+  if (selectedProvenance.value && isObjectHasKeys(selectedProvenance.value, ["prov"])) {
+    const result = await ProvService.getProvHistory(props.entityIri);
+    let matchIri = "";
+    result.forEach((p: any) => {
+      if (p["@id"] === selectedProvenance.value.prov) {
+        matchIri = p[IM.PROVENANCE_USED][0]["@id"].toString();
+      }
+    });
+    const a = await EntityService.getFullEntity(matchIri);
+    if (a[IM.DEFINITION]) jsonDisplay.value = a["@id"];
+  }
+});
 
 async function getProvHistory(iri: string) {
   loading.value = true;
@@ -37,7 +84,15 @@ async function getProvHistory(iri: string) {
       agent: p[IM.PROVENANCE_AGENT]?.[0].name || p[IM.PROVENANCE_AGENT]?.[0]["@id"] || "---"
     })
   );
+
   loading.value = false;
+}
+
+function onRowClick() {
+  visible.value = true;
+}
+function onClose() {
+  selectedProvenance.value = {};
 }
 </script>
 
