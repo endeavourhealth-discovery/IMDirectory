7988353f16d2fe4899e34e7439b2b5c54bbfef45
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2024-04-03 09:32:48 +0100

    added basic provenance UI display

--- ui/src/components/directory/viewer/Provenance.vue
+++ ui/src/components/directory/viewer/Provenance.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="provenance-container">
     <DataTable
-      @row-click="onRowClick()"
       :value="provenances"
       show-gridlines
       :scrollable="true"
@@ -22,16 +21,37 @@
     <Dialog
       v-model:visible="visible"
       modal
-      header="Test!"
+      :header="'Provenance details: ' + jsonDisplay"
+      :contentStyle="{ display: 'block' }"
       :style="{
-        minWidth: '90vw',
-        minHeight: '90vh',
-        display: 'flex',
-        flexFlow: 'column nowrap'
+        width: '90vw',
+        minHeight: '90vh'
       }"
       @hide="onClose()"
     >
-      <Panel header="JSON" toggleable>
+      <div v-for="(value, key) in provItem" class="prov-item">
+        <label v-html="getLabel(key)"></label>
+        <div v-if="!Array.isArray(value) && getLabel(key)">{{ value }}</div>
+        <div v-if="Array.isArray(value) && getLabel(key)">
+          <div v-for="v in value">
+            <div v-if="getLabel(key) === 'Properties:'">
+              <span v-if="isObjectHasKeys(v, [SHACL.PATH])">Property: {{ v[SHACL.PATH][0].name }}</span>
+              <span v-if="isObjectHasKeys(v, [SHACL.NODE])">, Range: {{ v[SHACL.NODE][0].name }} </span
+              ><span v-if="isObjectHasKeys(v, [IM.INHERITED_FROM])"> (inherited)</span>
+            </div>
+            <div v-else-if="getLabel(key) === 'Content type:'">
+              <span v-if="v.name">{{ v.name }}</span>
+            </div>
+            <div v-else-if="getLabel(key) === 'Term codes:'">
+              <span v-if="isObjectHasKeys(v, [RDFS.LABEL])">Name: {{ v[RDFS.LABEL] }}</span
+              ><span v-if="isObjectHasKeys(v, [IM.CODE])">, Code: {{ v[IM.CODE] }}</span
+              ><span v-if="isObjectHasKeys(v, [IM.HAS_STATUS])">, Status: {{ v[IM.HAS_STATUS][0].name }}</span>
+            </div>
+            <div v-else-if="v.name">{{ v.name }}</div>
+          </div>
+        </div>
+      </div>
+      <Panel class="provItem" header="JSON" toggleable collapsed>
         <JSONViewer :entity-iri="jsonDisplay"></JSONViewer>
       </Panel>
     </Dialog>
@@ -40,10 +60,11 @@
 
 <script setup lang="ts">
 import { EntityService, ProvService } from "@/services";
-import { IM, RDFS } from "@im-library/vocabulary";
+import { IM, RDF, RDFS, SHACL } from "@im-library/vocabulary";
 import { onMounted, ref, Ref, watch } from "vue";
 import JSONViewer from "@/components/directory/viewer/JSONViewer.vue";
 import { isObjectHasKeys } from "@im-library/helpers/DataTypeCheckers";
+import { setupEditorShape } from "@/composables/setupEditorShape";
 
 interface Props {
   entityIri: string;
@@ -53,22 +74,54 @@ const props = defineProps<Props>();
 const provenances: Ref<any[]> = ref([]);
 const loading: Ref<boolean> = ref(false);
 const visible = ref(false);
+
 const selectedProvenance = ref();
+const provItem = ref();
 const jsonDisplay = ref();
 
-onMounted(async () => await getProvHistory(props.entityIri));
+const labels = ref({
+  "@id": "Iri:",
+  [RDF.TYPE]: "Type:",
+  [RDFS.LABEL]: "Name:",
+  [RDFS.COMMENT]: "Description:",
+  [IM.HAS_STATUS]: "Status:",
+  [RDFS.SUBCLASS_OF]: "Subclass of:",
+  [IM.IS_CONTAINED_IN]: "Contained in:",
+  [IM.CODE]: "Code:",
+  [IM.VERSION]: "Version:",
+  [SHACL.PROPERTY]: "Properties:",
+  [IM.PREFERRED_NAME]: "Preferred name:",
+  [IM.CONTENT_TYPE]: "Content type:",
+  [IM.MATCHED_TO]: "Mapped to:",
+  [IM.IS_CHILD_OF]: "Child of:",
+  [IM.HAS_TERM_CODE]: "Term codes:"
+});
+
+onMounted(async () => {
+  await getProvHistory(props.entityIri);
+});
+
+const { shape, getShapesCombined, processShape, groups } = setupEditorShape();
 
 watch(selectedProvenance, async () => {
   if (selectedProvenance.value && isObjectHasKeys(selectedProvenance.value, ["prov"])) {
     const result = await ProvService.getProvHistory(props.entityIri);
     let matchIri = "";
     result.forEach((p: any) => {
-      if (p["@id"] === selectedProvenance.value.prov) {
+      if (p["@id"] === selectedProvenance.value.prov && p[IM.PROVENANCE_USED]) {
         matchIri = p[IM.PROVENANCE_USED][0]["@id"].toString();
       }
     });
-    const a = await EntityService.getFullEntity(matchIri);
-    if (a[IM.DEFINITION]) jsonDisplay.value = a["@id"];
+    if (matchIri === "") {
+      visible.value = false;
+    } else {
+      const entity = await EntityService.getEntityByPredicateExclusions(matchIri, [IM.HAS_MEMBER]);
+      if (isObjectHasKeys(entity, [IM.DEFINITION])) {
+        provItem.value = JSON.parse(entity[IM.DEFINITION]);
+        jsonDisplay.value = entity["@id"];
+      }
+      visible.value = true;
+    }
   }
 });
 
@@ -88,11 +141,13 @@ async function getProvHistory(iri: string) {
   loading.value = false;
 }
 
-function onRowClick() {
-  visible.value = true;
-}
 function onClose() {
   selectedProvenance.value = {};
+  visible.value = false;
+}
+
+function getLabel(key: any) {
+  return labels.value[key];
 }
 </script>
 
@@ -101,5 +156,14 @@ function onClose() {
   display: flex;
   flex-flow: column nowrap;
   width: 100%;
+}
+
+.prov-item {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+label {
+  font-weight: bold;
 }
 </style>
