ab9a243e09ea5576c5420cbd2448b810efb120bc
matt-voror <matt@voror.co.uk>
2024-04-23 16:06:39 +0100

    deleted ecl antlr, grammar, visitors. Migrated all api calls over to imapi versions.

--- ui/src/services/EclService.ts
+++ ui/src/services/EclService.ts
@@ -26,11 +26,11 @@ const EclService = {
   },
 
   async getQueryFromECL(ecl: string): Promise<Query> {
-    return axios.post(Env.VITE_NODE_API + "node_api/ecl/public/eclToIMQ", ecl, { headers: { "Content-Type": "text/plain" } });
+    return axios.post(Env.API + "api/ecl/public/queryFromEcl", ecl, { headers: { "Content-Type": "text/plain" } });
   },
 
   async isValidECL(ecl: string): Promise<boolean> {
-    return axios.post(Env.VITE_NODE_API + "node_api/ecl/public/validateEcl", ecl, { headers: { "Content-Type": "text/plain" } });
+    return axios.post(Env.API + "api/ecl/public/validateEcl", ecl, { headers: { "Content-Type": "text/plain" } });
   },
 
   async getECLFromQuery(query: Query, includeNames?: boolean): Promise<any> {
@@ -43,10 +43,6 @@ const EclService = {
       if (isObjectHasKeys(result, ["err"])) throw new Error(result.err);
       else return result;
     }
-  },
-
-  async getBuildFromEcl(ecl: string, raw: boolean = false): Promise<any> {
-    return axios.post(Env.VITE_NODE_API + "node_api/ecl/public/eclToBuilder", ecl, { raw: raw, headers: { "Content-Type": "text/plain" } });
   },
 
   async getEclBuilderFromQuery(query: Query, raw: boolean = false): Promise<BoolGroup> {
