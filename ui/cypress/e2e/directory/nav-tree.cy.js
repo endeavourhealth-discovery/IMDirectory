4b4904076f78e338b25cd93c4440aaa0c29163f2
matt-voror <matt@voror.co.uk>
2024-05-23 17:19:24 +0100

    additional nav tree tests

--- ui/cypress/e2e/directory/nav-tree.cy.js
+++ ui/cypress/e2e/directory/nav-tree.cy.js
@@ -27,4 +27,12 @@ describe("nav tree", () => {
       .find(".p-treenode")
       .should("have.length.at.least", 22);
   });
+  it("can select a node and route", () => {
+    cy.get("#hierarchy-tree-bar-container").contains("Ontologies").click();
+    cy.get("#directory-table-container", { timeout: 60000 }).find(".parent-header-container", { timeout: 10000 }).contains("Ontologies");
+  });
+  it("shows overlay on hover", () => {
+    cy.get("#hierarchy-tree-bar-container").contains("Ontologies").trigger("mouseover");
+    cy.get("#overlay-panel").contains("Ontologies");
+  });
 });
