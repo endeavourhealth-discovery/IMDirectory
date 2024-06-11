e27bf4feb31db6bd907f224238bec8d1dc80e11f
matt-voror <matt@voror.co.uk>
2024-06-11 15:38:48 +0100

    user-details tests added

---
+++ ui/cypress/e2e/auth/user-details.cy.js
@@ -0,0 +1,37 @@
+describe("User details", () => {
+  beforeEach(() => {
+    cy.login();
+    cy.getByTestId("account-menu-logged-in").click();
+    cy.get("#account-menu").contains("My account").click();
+    cy.get(".user-container", { timeout: 60000 });
+  });
+
+  it("starts with user data", () => {
+    cy.getByTestId("user-details-avatar").should("have.attr", "src").should("include", "001-man");
+    cy.getByTestId("user-details-username").should("have.value", Cypress.env("CYPRESS_LOGIN_USERNAME"));
+    cy.getByTestId("user-details-firstname").should("have.value", "Cypress");
+    cy.getByTestId("user-details-lastname").should("have.value", "Test");
+    cy.getByTestId("user-details-email").should("have.value", "test@example.com");
+  });
+
+  it("shows security status", () => {
+    cy.get(".menu-container").contains("Security").click();
+    cy.get(".two-factor-row").find(".p-tag-value").contains("Inactive");
+  });
+
+  it("can route to mfa setup", () => {
+    cy.get(".menu-container").contains("Security").click();
+    cy.get(".two-factor-row").find("button").click();
+    cy.get(".swal2-popup").find(".swal2-confirm").contains("Login").click();
+    cy.getByTestId("login-username").type(Cypress.env("CYPRESS_LOGIN_USERNAME"));
+    cy.getByTestId("login-password").type(Cypress.env("CYPRESS_LOGIN_PASSWORD"));
+    cy.getByTestId("login-submit").click();
+    cy.get(".swal2-popup").find(".swal2-confirm").click();
+    cy.url({ timeout: 60000 }).should("contain", "/mfa-setup");
+  });
+
+  it.only("an show authorisation roles", () => {
+    cy.get(".menu-container").contains("Authorisation roles").click();
+    cy.get(".roles-content").find("tr").should("have.length.at.least", 1);
+  });
+});
