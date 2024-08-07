706ea0ea7e467ef72c89a80781ff4a9682421608
Kostas Kardatos <kon.kardatos@gmail.com>
2024-08-07 15:29:46 +0100

    Title for path selection Fixes #2002

--- ui/src/components/imquery/addNewFeatureDialog/PathSelect.vue
+++ ui/src/components/imquery/addNewFeatureDialog/PathSelect.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="view-title"><b>Path</b></div>
   <Listbox v-model="localSelectedPath" :options="pathSuggestions" class="w-full" @change="onSelect()">
     <template #empty> No available paths </template>
     <template #option="{ option }: { option: Match }">
