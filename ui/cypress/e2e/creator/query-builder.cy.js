8dd6e686d7a63f6ec45c89a9978520d99e28a605
Kostas Kardatos <kon.kardatos@gmail.com>
2024-10-03 15:41:25 +0100

    add tests with different property types

--- ui/cypress/e2e/creator/query-builder.cy.js
+++ ui/cypress/e2e/creator/query-builder.cy.js
@@ -105,7 +105,7 @@ describe("Query builder", () => {
     cy.contains(".edit-match-container", "or").should("not.exist");
   });
 
-  it.only("add feature by searching for a set and adding a property", () => {
+  it("add feature by searching for a set and adding a date property", () => {
     cy.populateBaseType();
     cy.get(".add-feature-button").contains("Add feature").click();
     cy.get(".p-dialog-content").find("[data-testid=search-input]").type("diabetes");
@@ -113,13 +113,57 @@ describe("Query builder", () => {
     cy.get(".parent-header-container").find(".p-button-label").contains("Add").click();
     cy.wait(1000);
     cy.getByTestId("add-feature-ok-button").contains("OK").click();
-
     cy.get(".p-dialog-content").find(".add-property-button").click();
     cy.get(".p-tree-node-selectable").contains("date").click();
-    cy.get(".p-inputtext").contains("date").siblings().find(".p-date-picker");
-    // cy.getByTestId("add-feature-save-button").contains("Save").click();
-    // cy.get(".edit-match-container").contains("Condition");
-    // cy.get(".edit-match-container").contains("Diabetes");
+    cy.get(".datatype-select").find(".p-datepicker-input").click({ force: true });
+    cy.get(".p-datepicker-day-cell").contains("1").click();
+    cy.getByTestId("add-property-dialog-save").contains("Save").click();
+    cy.getByTestId("add-feature-save-button").contains("Save").click();
+    cy.get(".edit-match-container").contains("Condition");
+    cy.get(".edit-match-container").contains("Diabetes");
+    cy.get(".edit-match-container").contains("date");
+  });
+
+  it("add feature by searching for a set and adding a string property", () => {
+    cy.populateBaseType();
+    cy.get(".add-feature-button").contains("Add feature").click();
+    cy.get(".p-dialog-content").find("[data-testid=search-input]").type("diabetes");
+    cy.get(".datatable-flex-cell").contains("Diabetes mellitus type 2 or Diabetes mellitus in remission + more...").click();
+    cy.get(".parent-header-container").find(".p-button-label").contains("Add").click();
+    cy.wait(1000);
+    cy.getByTestId("add-feature-ok-button").contains("OK").click();
+    cy.get(".p-dialog-content").find(".add-property-button").click();
+    cy.get(".p-tree-node-selectable").contains("description").click();
+    cy.get(".datatype-select").find(".p-select-dropdown").click({ force: true });
+    cy.get(".p-select-option").contains("starts with").click();
+    cy.get(".datatype-select").find(".p-select-dropdown").parent().siblings().last().type("test desc");
+    cy.getByTestId("add-property-dialog-save").contains("Save").click();
+    cy.getByTestId("add-feature-save-button").contains("Save").click();
+    cy.get(".edit-match-container").contains("Condition");
+    cy.get(".edit-match-container").contains("Diabetes");
+    cy.get(".edit-match-container").contains("description");
+    cy.get(".edit-match-container").contains("test desc");
+  });
+
+  it("add feature by searching for a set and adding a numeric property", () => {
+    cy.populateBaseType();
+    cy.get(".add-feature-button").contains("Add feature").click();
+    cy.get(".p-dialog-content").find("[data-testid=search-input]").type("blood pressure");
+    cy.get(".datatable-flex-cell").first().click();
+    cy.get(".parent-header-container").find(".p-button-label").contains("Add").click();
+    cy.wait(1000);
+    cy.get(".p-listbox").last().find(".p-listbox-option").contains("patient -> Numeric measurement . concept").click();
+    cy.getByTestId("add-feature-ok-button").contains("OK").click();
+    cy.get(".p-dialog-content").find(".add-property-button").click();
+
+    cy.get(".p-tree-node-selectable").contains("value").click();
+    cy.get(".datatype-select").find("[placeholder=value]").type("140");
+    cy.getByTestId("add-property-dialog-save").contains("Save").click();
+    cy.getByTestId("add-feature-save-button").contains("Save").click();
+    cy.get(".edit-match-container").contains("Numeric measurement");
+    cy.get(".edit-match-container").contains("blood pressure");
+    cy.get(".edit-match-container").contains("value");
+    cy.get(".edit-match-container").contains("140");
   });
 
   it("add feature by searching for a concept", () => {
@@ -143,7 +187,7 @@ describe("Query builder", () => {
     cy.get(".p-dialog-content").find("[data-testid=search-input]").type("nhs number");
     cy.get(".datatable-flex-cell").contains("nhs number", { matchCase: false }).click();
     cy.getByTestId("add-feature-ok-button").contains("OK").click();
-    cy.get(".property-input-container", { timeout: 1000 }).find("[data-testid=property-value-input]").type("123456789");
+    cy.get(".property-input-container", { timeout: 6000 }).find("[data-testid=property-value-input]").type("123456789");
     cy.getByTestId("add-feature-save-button").contains("Save").click();
     cy.get(".edit-match-container").contains("Patient personal and demographics");
     cy.get(".edit-match-container").contains("nhs number 123456789", { matchCase: false });
@@ -155,7 +199,7 @@ describe("Query builder", () => {
     cy.get("#Feature").click();
     cy.get(".p-dialog-content").find("[data-testid=search-input]").type("diabetes");
     cy.get(".datatable-flex-cell").contains("Active Diabetes (Latest entry for diabetes not followed by a resolution)").click();
-    cy.getByTestId("add-feature-save-query-button", { timeout: 1000 }).contains("Save").click();
+    cy.getByTestId("add-feature-save-query-button", { timeout: 6000 }).contains("Save").click();
     cy.get(".edit-match-container").contains("in the cohort");
     cy.get(".edit-match-container").contains("Active Diabetes (Latest entry for diabetes not followed by a resolution)");
   });
@@ -166,7 +210,7 @@ describe("Query builder", () => {
     cy.get("#Cohort").click();
     cy.get(".p-dialog-content").find("[data-testid=search-input]").type("patient");
     cy.get(".datatable-flex-cell").contains("Patients registered for GMS services on the reference date").click();
-    cy.getByTestId("add-feature-save-query-button", { timeout: 1000 }).contains("Save").click();
+    cy.getByTestId("add-feature-save-query-button", { timeout: 6000 }).contains("Save").click();
     cy.get(".edit-match-container").contains("in the cohort");
     cy.get(".edit-match-container").contains("Patients registered for GMS services on the reference date");
   });
