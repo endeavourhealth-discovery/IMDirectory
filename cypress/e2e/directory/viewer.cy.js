e250f53067be91c36618ee2017be39c70c8d11a7
matt-voror <matt@voror.co.uk>
2024-11-12 21:03:49 +0000

    added download test. Added dateNowReverse and test

--- cypress/e2e/directory/viewer.cy.js
+++ cypress/e2e/directory/viewer.cy.js
@@ -1,3 +1,6 @@
+import { dateNowReverse } from "@/helpers/Datetime/DateNowReverse";
+import path from "path";
+
 describe("viewer", () => {
   beforeEach(() => {
     cy.acceptLicenseAndCookies();
@@ -14,6 +17,13 @@ describe("viewer", () => {
     it("can find in tree", () => {
       cy.get(".entity-buttons-container").find(".fa-list-tree").click();
       cy.get("#hierarchy-tree-bar-container").contains("Asthma", { timeout: 60000 });
+    });
+    it.only("can download concept", () => {
+      const currentDate = dateNowReverse("_");
+      const cypressDownloads = Cypress.config("downloadsFolder");
+      cy.getByTestId("download-button").click();
+      cy.get(".p-confirmdialog").find(".p-confirmdialog-accept-button").click();
+      cy.readFile(path.join(cypressDownloads + "/asthma (disorder)_" + currentDate + ".json"));
     });
   });
 });
