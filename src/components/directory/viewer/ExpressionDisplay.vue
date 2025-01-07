619fb99b3d010b6ba522cbe59e3e01cf528994e5
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2025-01-07 16:02:10 +0000

    fixed expression tab display logic

--- src/components/directory/viewer/ExpressionDisplay.vue
+++ src/components/directory/viewer/ExpressionDisplay.vue
@@ -2,7 +2,7 @@
   <div v-if="loading">
     <ProgressSpinner />
   </div>
-  <div v-else-if="isObjectHasKeys(concept, [RDFS.SUBCLASS_OF, IM.ROLE_GROUP])" id="expression-container">
+  <div v-else-if="isObjectHasKeys(concept, [RDFS.SUBCLASS_OF])" id="expression-container">
     <span
       class="ecl-text"
       data-testid="eclString"
@@ -21,6 +21,7 @@ import { isObjectHasKeys } from "@/helpers/DataTypeCheckers";
 import setupCopyToClipboard from "@/composables/setupCopyToClipboard";
 import { onMounted, ref } from "vue";
 import { IM, RDFS } from "@/vocabulary";
+import { EntityService } from "@/services";
 
 interface Props {
   concept: any;
@@ -36,76 +37,51 @@ onMounted(async () => await init());
 
 async function init() {
   loading.value = true;
-  generateExpression();
+  await generateExpression();
   loading.value = false;
 }
 
-function generateExpression() {
+async function generateExpression() {
   let result = "";
   if (props.concept[IM.DEFINITIONAL_STATUS] && props.concept[IM.DEFINITIONAL_STATUS].length) {
     for (const def of props.concept[IM.DEFINITIONAL_STATUS]) {
-      if (def["@id"] === "http://endhealth.info/im#1251000252106") result += "=== ";
+      if (def["@id"] === "http://endhealth.info/im#1251000252106") result += "===\t";
     }
   } else {
-    result += "<<< ";
+    result += "<<<\t";
   }
-  for (let subclass in props.concept[RDFS.SUBCLASS_OF]) {
-    result += props.concept[RDFS.SUBCLASS_OF][subclass]["@id"].split("#")[1] + " |" + props.concept[RDFS.SUBCLASS_OF][subclass].name + "|";
-    if (parseInt(subclass) < props.concept[RDFS.SUBCLASS_OF].length - 1) result += " +\n\t\t";
-  }
-  result += " :\n\t\t";
-
-  for (let roleGroup of props.concept[IM.ROLE_GROUP]) {
-    const newGroup = roleGroup;
-    delete newGroup[IM.GROUP_NUMBER];
-    for (let key of newGroup) {
+  if (props.concept[RDFS.SUBCLASS_OF]) {
+    for (let subclass in props.concept[RDFS.SUBCLASS_OF]) {
+      result += props.concept[RDFS.SUBCLASS_OF][subclass]["@id"].split("#")[1] + " |" + props.concept[RDFS.SUBCLASS_OF][subclass].name + "|";
+      if (parseInt(subclass) < props.concept[RDFS.SUBCLASS_OF].length - 1) result += " +\n\t";
     }
   }
 
-  //console.log(result.length);
-  if (result.length) expression.value = result;
-  else expression.value = "Error creating expression.";
-
-  /* const instanceArray = [];
-  for (let subclass of concept.value[RDFS.SUBCLASS_OF]) {
-    const instanceOf = { "@id": subclass["@id"], name: subclass.name, descendantsOrSelfOf: true };
-    instanceArray.push(instanceOf);
-  }
-
-  let rg = { "@id": IM.ROLE_GROUP, match: { where: [] as Where[] } } as Where;
-  let whereRg = [] as Where[];
-  for (let roleGroup of concept.value[IM.ROLE_GROUP]) {
-    let subWhere = { boolWhere: Bool.and, where: [] as Where[] } as Where;
-    const newGroups = roleGroup;
-    delete newGroups[IM.GROUP_NUMBER];
-    for (let key in newGroups) {
-      let roleGroups = await EntityService.getPartialEntity(key, [RDFS.LABEL]);
-      if (Object.keys(newGroups).length === 1) {
-        rg.match!.where!.push({
-          "@id": key,
-          name: roleGroups[RDFS.LABEL],
-          is: [{ "@id": newGroups[key][0]["@id"], name: newGroups[key][0].name }],
-          anyRoleGroup: false
-        });
-      } else {
-        subWhere.where!.push({
-          "@id": key,
-          name: roleGroups[RDFS.LABEL],
-          is: [{ "@id": newGroups[key][0]["@id"], name: newGroups[key][0].name }],
-          anyRoleGroup: false
-        });
+  if (props.concept[IM.ROLE_GROUP] && props.concept[IM.ROLE_GROUP].length) {
+    result += " :\n";
+    for (let roleGroup of props.concept[IM.ROLE_GROUP]) {
+      const newGroup = roleGroup;
+      delete newGroup[IM.GROUP_NUMBER];
+      let count = 0;
+      for (const key of Object.keys(newGroup)) {
+        const value = newGroup[key];
+        const label = await EntityService.getPartialEntity(key, [RDFS.LABEL]);
+        if (count === 0) {
+          result += "\t\t{ ";
+        } else result += "\n\t\t  ";
+        count++;
+        result += key.split("#")[1] + " |" + label[RDFS.LABEL] + "| = " + value[0]["@id"].split("#")[1] + " |" + value[0].name + "|";
+        if (count < Object.entries(newGroup).length) result += ",";
       }
-    }
-    if (Object.keys(newGroups).length !== 1) {
-      rg.match!.boolMatch = Bool.or;
-      rg.match!.where!.push(subWhere);
+      count = 0;
+      result += " }\n";
     }
   }
-  const roleGroupArray = [];
-  roleGroupArray.push(rg);
-  test.value = { where: roleGroupArray, instanceOf: instanceArray };
-  console.log(JSON.stringify(test.value));
-  //concept.value[IM.DEFINITION] = JSON.stringify(test.value);*/
+  if (result.length) {
+    expression.value = result;
+  } else {
+    expression.value = "Error creating expression.";
+  }
 }
 </script>
 
@@ -113,6 +89,9 @@ function generateExpression() {
 .ecl-text {
   white-space: break-spaces;
   cursor: pointer;
+  font-family: "Courier New", monospace;
+  font-weight: 700;
+  font-size: 13px;
 }
 
 .loading-container {
