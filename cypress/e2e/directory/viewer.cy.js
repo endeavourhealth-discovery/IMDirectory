eae8179d49d9c4c337e3fd278716d0eddd45340b
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2025-01-08 17:27:12 +0000

    fix for viewer tab navigation

--- cypress/e2e/directory/viewer.cy.js
+++ cypress/e2e/directory/viewer.cy.js
@@ -174,6 +174,7 @@ describe("viewer", () => {
       });
       describe("graph tab", () => {
         beforeEach(() => {
+          cy.get(".p-tablist-next-button").click();
           cy.get(".p-tablist-tab-list").find(".p-tab").contains("Graph").click();
         });
         it("loads d3 graph for entity", () => {
@@ -182,6 +183,7 @@ describe("viewer", () => {
       });
       describe("json tab", () => {
         beforeEach(() => {
+          cy.get(".p-tablist-next-button").click();
           cy.get(".p-tablist-next-button").click();
           cy.get(".p-tablist-next-button").click();
           cy.get("#viewer-tabs").contains("JSON").click();
