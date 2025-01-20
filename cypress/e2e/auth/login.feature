da4f19a09c81d6e8300c6b1dc9c95e31ce8decdd
Richard Collier <rich@voror.co.uk>
2025-01-20 11:12:39 +0000

    IMDirectory-2400 Token not initialized in private mode Rework e2e and add test for this fix

--- cypress/e2e/auth/login.feature
+++ cypress/e2e/auth/login.feature
@@ -2,61 +2,71 @@ Feature: login
   Scenario: Admin menu
     When I visit the home page
     And I accept the license and cookies
-    Then I should see the account menu
+    Then I see the account menu
 
   Scenario: Login option
     When I visit the home page
     And I accept the license and cookies
     And I click on the account menu
-    Then I should see login option
+    Then I see login option
 
   Scenario: Login page
     When I visit the home page
     And I accept the license and cookies
     And I click on the account menu
     And I click on the login option
-    Then I should see the login page
+    Then I see the login page
 
   Scenario: Login
     When I navigate to the login page
-    And I Enter a valid username
-    And I Enter a valid password
+    And I enter a valid username
+    And I enter a valid password
     And I click on the login button
-    Then I should see the login confirmation
+    Then I see the login confirmation
 
   Scenario: Reveal password
     When I navigate to the login page
-    And I Enter a valid password
+    And I enter a valid password
     And I click reveal password
-    Then I should see the password
+    Then I see the password
 
   Scenario: Register new account
     When I navigate to the login page
-    Then I should be able to navigate to register an account
+    Then I be able to navigate to register an account
 
-  Scenario: Enter confirmation code
+  Scenario: enter confirmation code
     When I navigate to the login page
-    Then I should be able to navigate to enter a confirmation code
+    Then I be able to navigate to enter a confirmation code
 
   Scenario: Recover account
     When I navigate to the login page
-    Then I should be able to navigate to recover my account
+    Then I be able to navigate to recover my account
 
   Scenario: Private mode home page
     Given the server is in private mode
     When I visit the home page
     And I accept the license and cookies
-    Then I should see the login page
+    Then I see the login page
 
   Scenario: Private mode direct url
     Given the server is in private mode
     When I accept the license and cookies
     And I navigate to a concept page
-    Then I should see the login page
+    Then I see the login page
 
   Scenario: Private mode hidden navigation
     Given the server is in private mode
     When I visit the home page
     And I accept the license and cookies
-    Then I should see the login page
+    Then I see the login page
     And the home and back buttons are not available
+
+  Scenario: Private mode remain logged in on refresh
+    Given the server is in private mode
+    When I visit the home page
+    And I accept the license and cookies
+    And I login to the application
+    Then I see the login confirmation
+    When I visit the home page
+    And I refresh the page
+    Then I see the home page
--- cypress/e2e/auth/login.js
+++ cypress/e2e/auth/login.js
@@ -10,20 +10,20 @@ Given("the server is in private mode", () => {
   });
 });
 
-When("I visit the home page", function () {
+When("I visit the home page", () => {
   cy.visit("/");
 });
 
-When("I accept the license and cookies", function () {
+When("I accept the license and cookies", () => {
   cy.acceptLicenseAndCookies();
 });
 
-When("I click on the account menu", function () {
+When("I click on the account menu", () => {
   cy.get("#topbar", { timeout: 60000 });
   cy.getByTestId("account-menu").click();
 });
 
-When("I click on the login option", function () {
+When("I click on the login option", () => {
   cy.get("#account-menu").find("span").contains("Login").click();
 });
 
@@ -31,64 +31,75 @@ When("I navigate to a concept page", () => {
   cy.visit("/#/directory/folder/http%3A%2F%2Fsnomed.info%2Fsct%23195967001");
 });
 
-When("I navigate to the login page", function () {
+When("I navigate to the login page", () => {
   Step(this, "I visit the home page");
   Step(this, "I accept the license and cookies");
   Step(this, "I click on the account menu");
   Step(this, "I click on the login option");
 });
 
-When("I Enter a valid username", function () {
+When("I login to the application", () => {
+  Step(this, "I enter a valid username");
+  Step(this, "I enter a valid password");
+  Step(this, "I click on the login button");
+});
+
+When("I enter a valid username", () => {
   cy.getByTestId("login-username").type(Cypress.env("CYPRESS_LOGIN_USERNAME"));
 });
 
-When("I Enter a valid password", function () {
+When("I enter a valid password", () => {
   cy.getByTestId("login-password").type(Cypress.env("CYPRESS_LOGIN_PASSWORD"));
 });
 
-When("I click on the login button", function () {
+When("I click on the login button", () => {
   cy.getByTestId("login-submit").click();
 });
 
-When("I click reveal password", function () {
+When("I click reveal password", () => {
   cy.get("#fieldPassword").find("svg").click();
 });
 
-Then("I should see the account menu", () => {
+When("I refresh the page", () => {
+  cy.reload();
+});
+
+Then("I see the account menu", () => {
   cy.get("#topbar", { timeout: 60000 });
   cy.getByTestId("account-menu").should("exist");
 });
 
-Then("I should see login option", () => {
+Then("I see login option", () => {
   cy.get("#account-menu").find("span").should("exist");
 });
 
-Then("I should see the login page", () => {
+Then("I see the login page", () => {
   cy.get('[data-testid="login-submit"]').should("exist");
-  // cy.url().should("include", "/user/login");
 });
 
-Then("I should see the login confirmation", () => {
-  cy.get(".swal2-popup", { timeout: 60000 }).contains("Login successful");
-  cy.get(".swal2-confirm").click();
+Then("I see the home page", () => {
   cy.get("#topbar", { timeout: 60000 });
 });
 
-Then("I should see the password", () => {
+Then("I see the login confirmation", () => {
+  cy.get(".swal2-popup", { timeout: 60000 }).contains("Login successful");
+});
+
+Then("I see the password", () => {
   cy.getByTestId("login-password").should("have.value", Cypress.env("CYPRESS_LOGIN_PASSWORD"));
 });
 
-Then("I should be able to navigate to register an account", () => {
+Then("I be able to navigate to register an account", () => {
   cy.get("#register-link").click();
   cy.url().should("include", "/user/register");
 });
 
-Then("I should be able to navigate to enter a confirmation code", () => {
+Then("I be able to navigate to enter a confirmation code", () => {
   cy.get("#code-link").click();
   cy.url().should("include", "/user/confirm-code");
 });
 
-Then("I should be able to navigate to recover my account", () => {
+Then("I be able to navigate to recover my account", () => {
   cy.get("#recover-link").click();
   cy.url().should("include", "/user/password-recovery");
 });
