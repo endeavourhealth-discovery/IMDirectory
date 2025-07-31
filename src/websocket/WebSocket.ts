c5124f7656afff68f778b1086f1704f30bbae4bc
matt-voror <matt@voror.co.uk>
2025-07-31 09:53:05 +0100

    WIP: setting up user specific websocket

---
+++ src/websocket/WebSocket.ts
@@ -0,0 +1,41 @@
+import SockJS from "sockjs-client";
+import { Client, IMessage, StompSubscription } from "@stomp/stompjs";
+import { Env } from "@/services";
+
+let stompClient: Client | null = null;
+let subscription: StompSubscription | null = null;
+
+export function connectWebSocket(onMessage: (msg: any) => void): void {
+  const socket = new SockJS(Env.API + "/websocket");
+
+  stompClient = new Client({
+    webSocketFactory: () => socket,
+    debug: str => console.log("[STOMP]", str),
+    onConnect: () => {
+      console.log("[WebSocket] connected");
+      subscription = stompClient!.subscribe("/topic/userQueryQueue", (message: IMessage) => {
+        try {
+          const data = JSON.parse(message.body);
+          onMessage(data);
+        } catch (err) {
+          console.error("Failed to parse message", err);
+        }
+      });
+    },
+    onStompError: frame => {
+      console.error("[Websocket] STOMP error:", frame);
+    }
+  });
+
+  stompClient.activate();
+}
+
+export function disconnectWebSocket(): void {
+  if (subscription) {
+    subscription.unsubscribe();
+  }
+  if (stompClient) {
+    stompClient.deactivate();
+    console.log("[Websocket] Disconnected");
+  }
+}
