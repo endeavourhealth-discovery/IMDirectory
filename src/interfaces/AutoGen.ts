99a21ea85f1ba2c1779b81c8b46ed0d5304cb946
David Stables <DavidStables@users.noreply.github.com>
2025-10-10 15:00:16 +0100

    Update AutoGen.ts

--- src/interfaces/AutoGen.ts
+++ src/interfaces/AutoGen.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-10-10 14:09:40.
+// Generated using typescript-generator version 3.2.1263 on 2025-10-10 14:59:11.
 
 export interface ConceptContextMap {
     id?: string;
