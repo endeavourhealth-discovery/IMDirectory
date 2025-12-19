30e86e3612829ba1391193a574fa2731a72bdfba
Richard Collier <richard.collier@voror.co.uk>
2025-12-19 17:41:01 +0000

    SetService test fix

--- tests/unit/services/SetService.spec.ts
+++ tests/unit/services/SetService.spec.ts
@@ -21,7 +21,7 @@ describe("SetService.ts ___ axios success", () => {
   it("can get IMV1", async () => {
     const result = await SetService.IMV1("testIri");
     expect(axios.get).toBeCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(Env.API + "api/set/public/export", { params: { iri: "testIri" }, responseType: "blob" });
+    expect(axios.get).toHaveBeenCalledWith(Env.API + "api/set/private/export", { params: { iri: "testIri" }, responseType: "blob" });
     expect(result).toBe("axios get return");
   });
 });
