59c200a42d97c93fb02e129b016e91bbf69d83a1
David Stables <DavidStables@users.noreply.github.com>
2025-12-21 13:06:52 +0000

    Update ResultsTable.vue

--- src/components/shared/ResultsTable.vue
+++ src/components/shared/ResultsTable.vue
@@ -227,6 +227,7 @@ async function onSearch() {
   searchLoading.value = false;
   if (!props.eclQuery && lastSearchTerm === props.searchTerm && page.value == 0) {
     let offset = undefined;
+    let lefttoDo = 0;
     if (response?.entities) {
       offset = response.entities.length;
     }
