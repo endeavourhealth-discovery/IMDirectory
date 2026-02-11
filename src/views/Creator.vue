71a6864f579feb9bc64ed3c70e2b8d20ce8482fb
matt-voror <matt@voror.co.uk>
2026-02-11 20:33:44 +0000

    added missing namespace to userStore and updated creator/editor to use namespace

--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -190,7 +190,7 @@ onUnmounted(() => {
 
 onMounted(async () => {
   loading.value = true;
-  if (currentUser.value && currentUser.value.organisations.length < 1) {
+  if (currentUser.value && currentUser.value.namespaces.length < 1) {
     await router.push({ name: "AccessDenied" });
   }
   await filterStore.fetchFilterSettings();
