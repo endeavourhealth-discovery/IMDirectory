918128a3579478845972830bf502a134802e9079
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2026-04-14 16:58:34 +0100

    fixed sign in redirect and properties tab

--- src/router/methods/intercepts.ts
+++ src/router/methods/intercepts.ts
@@ -1,6 +1,7 @@
 import { useSharedStore } from "@/stores/sharedStore";
 import Swal, { SweetAlertResult } from "sweetalert2";
 import { Router } from "vue-router";
+import { SecurityService } from "@/services";
 
 export async function directToLogin(router: Router) {
   const sharedStore = useSharedStore();
@@ -14,6 +15,9 @@ export async function directToLogin(router: Router) {
   }).then(async (result: SweetAlertResult) => {
     if (result.isConfirmed) {
       console.log("redirecting to login");
+      if (!sharedStore.signinUrl) {
+        sharedStore.updateSigninUrl(await SecurityService.getLoginUrl());
+      }
       window.location.href = sharedStore.signinUrl;
     } else {
       console.log("redirecting to landing page");
